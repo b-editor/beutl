0978ef8a5fa174d3699314f254574b6f0ffb5367
indigo-san <yuuto20060124@gmail.com>
2020-11-28 11:09:39 +0900

    [change] Buttonの購読をSubscribeに変更 close #14

--- BEditor/BEditor.Core/Data/Primitive/Components/ButtonCompoent.cs
+++ BEditor/BEditor.Core/Data/Primitive/Components/ButtonCompoent.cs
@@ -9,18 +9,59 @@ using BEditor.Core.Data.Property;
 
 namespace BEditor.Core.Data.Primitive.Components
 {
-    public class ButtonComponent : ComponentElement<PropertyElementMetadata>
+    public class ButtonComponent : ComponentElement<PropertyElementMetadata>, IObservable<object>
     {
+        private List<IObserver<object>> list;
+
         public ButtonComponent(PropertyElementMetadata metadata)
         {
             PropertyMetadata = metadata ?? throw new ArgumentNullException(nameof(metadata));
         }
 
-        public event EventHandler Click;
+        private List<IObserver<object>> Collection => list ??= new();
+
+        public IDisposable Subscribe(IObserver<object> observer)
+        {
+            Collection.Add(observer);
+            return Disposable.Create(() => Collection.Remove(observer));
+        }
+        public IDisposable Subscribe(Action onNext)
+        {
+            var observer = new Observer()
+            {
+                _OnNext = _ => onNext?.Invoke()
+            };
+            Collection.Add(observer);
+            return Disposable.Create(() => Collection.Remove(observer));
+        }
 
         public void Execute()
         {
-            Click?.Invoke(this, EventArgs.Empty);
+            // Rxの拡張メソッド対策
+            var tmp = new object();
+            foreach (var observer in Collection)
+            {
+                try
+                {
+                    observer.OnNext(tmp);
+                    observer.OnCompleted();
+                }
+                catch (Exception ex)
+                {
+                    observer.OnError(ex);
+                }
+            }
+        }
+
+        private class Observer : IObserver<object>
+        {
+            public Action<object> _OnNext;
+            public Action<Exception> _OnError;
+            public Action _OnCompleted;
+
+            public void OnCompleted() => _OnCompleted?.Invoke();
+            public void OnError(Exception error) => _OnError?.Invoke(error);
+            public void OnNext(object value) => _OnNext?.Invoke(value);
         }
     }
 }
