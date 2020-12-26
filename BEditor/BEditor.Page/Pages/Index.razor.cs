7fc5949472f6aca16abe03c481e08252d25e8228
indigo-san <yuuto20060124@gmail.com>
2020-12-26 21:29:19 +0900

    [update] gh-pagesのマークダウンをリポジトリのREADMEを参照する仕様に変更

--- BEditor/BEditor.Page/Pages/Index.razor.cs
+++ BEditor/BEditor.Page/Pages/Index.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 using Markdig;
@@ -9,40 +10,12 @@ namespace BEditor.Page.Pages
 {
     public partial class Index
     {
-        public Index()
+        protected override async Task OnInitializedAsync()
         {
-            CardText = Markdown.ToHtml(@$"
-# License
+            await base.OnInitializedAsync();
 
-* [OpenCV]({Consts.OpenCV_License_Link})
-* [OpenToolKit]({Consts.OpenTK_License_Link})
-* [BEditor]({Consts.BEditor_License_Link})
-
-# News
-
-## BEditor 0.0.3
-
-* [ダウンロード]({Consts.BEditor_003_Link})
-* [マイルストーン]({Consts.BEditor_003_MileStone_Link})
-
-### 変更
-
-* ランタイムが.NET5になりました
-* 3Dオブジェクトの追加
-* シーン, クリップを管理する ObjectViewer を追加
-* クリッピング, 領域拡張エフェクトの追加
-* 光源エフェクトの追加
-
-
-## BEditor 0.0.2
-
-* [ダウンロード]({Consts.BEditor_002_Link})
-
-## BEditor 0.0.1
-
-* [ダウンロード]({Consts.BEditor_001_Link})
-
-");
+            using var client = new HttpClient();
+            CardText = Markdown.ToHtml(await client.GetStringAsync("https://raw.githubusercontent.com/indigo-san/BEditor/main/README.md"));
         }
 
         public string CardText { get; set; }
