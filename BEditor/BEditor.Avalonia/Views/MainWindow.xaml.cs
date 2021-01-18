ac483d8a4ee090691f3a94353dc301b04dfdd762
indigo-san <lugongdao07@gmail.com>
2021-01-18 18:32:22 +0900

    [add] avaloniaのプロジェクト追加

---
+++ BEditor/BEditor.Avalonia/Views/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+using Material.Styles;
+
+namespace BEditor.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public void MenuButton_Click(object sender, RoutedEventArgs args)
+        {
+            var button = (Button)sender;
+
+            button.ContextMenu?.Open(button);
+        }
+    }
+}
