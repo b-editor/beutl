39aa8e2d8f0630590bdbccc68b2d317c1f129bbb
indigo-san <lugongdao07@gmail.com>
2021-01-22 23:25:32 +0900

    [update] BEditor.Extension.ObjectsをBEditor.Extension.AviUtlに変更し別のリポジトリに移動

--- BEditor/Tests/NUnitTestProject1/ExoTest.cs
+++ BEditor/Tests/NUnitTestProject1/ExoTest.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using BEditor.Extensions.Object;
+using BEditor.Extensions.AviUtl;
 
 using Microsoft.Extensions.Configuration;
 
