cf95d2254524169d468800918fd8a4926cbdfb5b
indigo-san <lugongdao07@gmail.com>
2021-05-20 01:12:35 -0700

    [fix] LinuxでFontManagerが動かないので修正

--- src/BEditor.Avalonia/CustomFontManagerImpl.cs
+++ src/BEditor.Avalonia/CustomFontManagerImpl.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
 
 using Avalonia.Media;
@@ -16,7 +17,11 @@ namespace BEditor
         private readonly string _defaultFamilyName;
 
         //Load font resources in the project, you can load multiple font resources
-        private readonly Typeface _defaultTypeface = new("resm:BEditor.Assets.Fonts.NotoSansJP-Regular#Noto Sans JP");
+        private readonly Typeface _defaultTypeface = new(MatchFace().FamilyName);
+        private static readonly string[] _familynames =
+        {
+            "Yu Gothic UI", "Segoe UI", "Hiragino Sans", "Helvetica Neue", "Ubuntu"
+        };
 
         public CustomFontManagerImpl()
         {
@@ -61,14 +66,40 @@ namespace BEditor
 
         public IGlyphTypefaceImpl CreateGlyphTypeface(Typeface typeface)
         {
-            using var stream = Assembly.GetExecutingAssembly()!.GetManifestResourceStream("BEditor.Assets.Fonts.NotoSansJP-Regular.otf");
-            var skTypeface = typeface.FontFamily.Name switch
-            {
-                FontFamily.DefaultFontFamilyName or "Noto Sans JP" or "Inter" => SKTypeface.FromStream(stream),
-                _ => SKTypeface.FromFamilyName(typeface.FontFamily.Name, (SKFontStyleWeight)typeface.Weight, SKFontStyleWidth.Normal, (SKFontStyleSlant)typeface.Style),
-            };
+            if (typeface.FontFamily.Name is "Inter") return new GlyphTypefaceImpl(MatchFace());
 
-            return new GlyphTypefaceImpl(skTypeface);
+            foreach (var name in GetInstalledFontFamilyNames())
+            {
+                foreach (var typefaces in typeface.FontFamily.FamilyNames)
+                {
+                    if (name == typefaces)
+                    {
+                        return new GlyphTypefaceImpl(SKTypeface.FromFamilyName(name, (SKFontStyleWeight)typeface.Weight, SKFontStyleWidth.Normal, (SKFontStyleSlant)typeface.Style));
+                    }
+                }
+            }
+
+            using var stream = Assembly.GetExecutingAssembly()!.GetManifestResourceStream("BEditor.Assets.Fonts.NotoSansJP-Regular.otf");
+
+            return new GlyphTypefaceImpl(SKTypeface.FromStream(stream));
+        }
+
+        private static SKTypeface MatchFace()
+        {
+            foreach (var name in SKFontManager.Default.FontFamilies)
+            {
+                foreach (var typefaces in _familynames)
+                {
+                    if (name == typefaces)
+                    {
+                        return SKTypeface.FromFamilyName(name, SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright);
+                    }
+                }
+            }
+
+            using var stream = Assembly.GetExecutingAssembly()!.GetManifestResourceStream("BEditor.Assets.Fonts.NotoSansJP-Regular.otf");
+
+            return SKTypeface.FromStream(stream);
         }
     }
 }
