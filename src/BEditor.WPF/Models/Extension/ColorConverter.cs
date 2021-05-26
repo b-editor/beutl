c964ff392c84cd0ba27749ad31ab9eba9e7551d8
indigo-san <lugongdao07@gmail.com>
2021-05-26 18:48:16 +0900

    [remove] Windows限定のプロジェクトを削除

--- src/BEditor.WPF/Models/Extension/ColorConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Media;
-
-namespace BEditor.Models.Extension
-{
-    public static class ColorConverter
-    {
-        public static Brush ToBrush(this BEditor.Drawing.Color color) => new SolidColorBrush(System.Windows.Media.Color.FromArgb((byte)color.A, (byte)color.R, (byte)color.G, (byte)color.B));
-    }
-}
--- src/BEditor.WPF/Models/Setting.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BEditor.Models
-{
-    public class Setting
-    {
-        public static double ClipHeight { get; } = Settings.Default.ClipHeight;
-        public static float WidthOf1Frame { get; } = Settings.Default.WidthOf1Frame;
-    }
-}
