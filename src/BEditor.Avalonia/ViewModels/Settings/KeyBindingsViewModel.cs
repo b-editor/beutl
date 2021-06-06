62d9c1868557fb4a4e926d199e6cf2fa1756d2e6
indigo-san <lugongdao07@gmail.com>
2021-06-07 00:45:45 +0900

    [update] ショートカットの編集画面を追加

---
+++ src/BEditor.Avalonia/ViewModels/Settings/KeyBindingsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Avalonia.Input;
+
+using BEditor.Models;
+
+using Reactive.Bindings;
+
+namespace BEditor.ViewModels.Settings
+{
+    public sealed class KeyBindingsViewModel
+    {
+        public KeyBindingsViewModel()
+        {
+            SelectedBinding = SelectedCommand
+                .Select(i => KeyBindingModel.Bindings.FirstOrDefault(c => c.CommandName == i.Name))
+                .ToReadOnlyReactivePropertySlim();
+
+            Key = SelectedBinding.Select(i => i?.Key).ToReactiveProperty();
+
+            Apply.Where(_ => SelectedBinding.Value is not null)
+                .Subscribe(_ =>
+                {
+                    if (Key.Value is not null)
+                    {
+                        SelectedBinding.Value!.Key = Key.Value;
+                    }
+                });
+        }
+
+        public KeyBindingCommand[] Commands { get; } = KeyBindingModel.AllCommands;
+
+        public ReactiveProperty<KeyBindingCommand> SelectedCommand { get; } = new(KeyBindingModel.AllCommands[0]);
+
+        public ReadOnlyReactivePropertySlim<KeyBindingModel?> SelectedBinding { get; }
+
+        public ReactiveProperty<string?> Key { get; }
+
+        public ReactiveCommand Apply { get; } = new();
+    }
+}
