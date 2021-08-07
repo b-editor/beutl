c98884fb2ce5cfd371f4a2270486893d87f65f50
indigo-san <lugongdao07@gmail.com>
2021-08-07 16:08:23 +0900

    [update] エフェクトのテストを追加

--- test/DrawingTest/LookupTableTest.cs
+++ test/DrawingTest/LookupTableTest.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 
 using BEditor.Drawing;
+using BEditor.Drawing.Pixel;
 
 using NUnit.Framework;
 
@@ -8,13 +9,57 @@ namespace DrawingTest
 {
     public class LookupTableTest
     {
+        public const string FilePath = "../../../../../docs/example/original.png";
+
         [Test]
         public void FromStream()
         {
-            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("DrawingTest.Resources.PB_Basin.CUBE");
-            var table = LookupTable.FromStream(stream);
+            using var img = Image<BGRA32>.FromFile(FilePath);
 
-            table.Dispose();
+            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("DrawingTest.Resources.PB_Basin.CUBE");
+            using var table = LookupTable.FromStream(stream);
+
+            img.Apply(table, 0.5f);
+        }
+
+        [Test]
+        public void Contrast()
+        {
+            using var img = Image<BGRA32>.FromFile(FilePath);
+
+            using var table = LookupTable.Contrast(255);
+
+            img.Apply(table);
+        }
+
+        [Test]
+        public void Gamma()
+        {
+            using var img = Image<BGRA32>.FromFile(FilePath);
+
+            using var table = LookupTable.Gamma(3);
+
+            img.Apply(table);
+        }
+
+        [Test]
+        public void Negaposi()
+        {
+            using var img = Image<BGRA32>.FromFile(FilePath);
+
+            using var table = LookupTable.Negaposi(255);
+
+            img.Apply(table);
+        }
+
+        [Test]
+        public void Solarisation()
+        {
+            using var img = Image<BGRA32>.FromFile(FilePath);
+
+            using var table = LookupTable.Solarisation(5);
+
+            img.Apply(table);
         }
     }
 }
