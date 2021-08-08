4cf384bf1cb712382928d92301462ac67b377a17
indigo-san <lugongdao07@gmail.com>
2021-08-08 19:30:09 +0900

    [update] タイトルバーにスタイルを適用

--- src/executable/BEditor.Avalonia/Views/CustomTitlebars/WindowsTitlebar.axaml.cs
+++ src/executable/BEditor.Avalonia/Views/CustomTitlebars/WindowsTitlebar.axaml.cs
@@ -30,40 +30,19 @@ namespace BEditor.Views.CustomTitlebars
 {
     public sealed class WindowsTitlebar : UserControl
     {
-        private readonly Button _minimizeButton;
-        private readonly Button _maximizeButton;
-        private readonly Path _maximizeIcon;
-        private readonly ToolTip _maximizeToolTip;
-        private readonly Button _closeButton;
         private readonly Menu _menu;
         private readonly MenuItem _recentFiles;
-        private readonly StackPanel _titlebarbuttons;
+        private readonly WindowsTitlebarButtons _titlebarbuttons;
 
         public WindowsTitlebar()
         {
             InitializeComponent();
-            _minimizeButton = this.FindControl<Button>("MinimizeButton");
-            _maximizeButton = this.FindControl<Button>("MaximizeButton");
-            _maximizeIcon = this.FindControl<Path>("MaximizeIcon");
-            _maximizeToolTip = this.FindControl<ToolTip>("MaximizeToolTip");
-            _closeButton = this.FindControl<Button>("CloseButton");
             _menu = this.FindControl<Menu>("menu");
             _recentFiles = this.FindControl<MenuItem>("RecentFiles");
-            _titlebarbuttons = this.FindControl<StackPanel>("titlebarbuttons");
+            _titlebarbuttons = this.FindControl<WindowsTitlebarButtons>("titlebarbuttons");
 
-            if (OperatingSystem.IsWindows())
-            {
-                SetRecentUsedFiles();
-                _minimizeButton.Click += MinimizeWindow;
-                _maximizeButton.Click += MaximizeWindow;
-                _closeButton.Click += CloseWindow;
-
-                PointerPressed += WindowsTitlebar_PointerPressed;
-
-                _menu.MenuOpened += (s, e) => PointerPressed -= WindowsTitlebar_PointerPressed;
-                _menu.MenuClosed += (s, e) => PointerPressed += WindowsTitlebar_PointerPressed;
-            }
-            else
+            SetRecentUsedFiles();
+            if (!OperatingSystem.IsWindows())
             {
                 _titlebarbuttons.IsVisible = false;
             }
@@ -265,69 +244,6 @@ namespace BEditor.Views.CustomTitlebars
             {
                 var dialog = new AddEffect { DataContext = new AddEffectViewModel() };
                 await dialog.ShowDialog(window);
-            }
-        }
-
-        public void WindowsTitlebar_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
-        {
-            if (VisualRoot is Window window)
-            {
-                window.BeginMoveDrag(e);
-            }
-        }
-
-        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            base.OnAttachedToVisualTree(e);
-
-            if (!OperatingSystem.IsWindows()) return;
-
-            ((Window)e.Root).GetObservable(Window.WindowStateProperty).Subscribe(s =>
-            {
-                var window = (Window)e.Root;
-                if (s is not WindowState.Maximized)
-                {
-                    _maximizeIcon.Data = Avalonia.Media.Geometry.Parse("M2048 2048v-2048h-2048v2048h2048zM1843 1843h-1638v-1638h1638v1638z");
-                    window.Padding = new Thickness(0, 0, 0, 0);
-                    _maximizeToolTip.Content = "Maximize";
-                }
-                if (s is WindowState.Maximized)
-                {
-                    _maximizeIcon.Data = Avalonia.Media.Geometry.Parse("M2048 1638h-410v410h-1638v-1638h410v-410h1638v1638zm-614-1024h-1229v1229h1229v-1229zm409-409h-1229v205h1024v1024h205v-1229z");
-                    window.Padding = new Thickness(7, 7, 7, 7);
-                    _maximizeToolTip.Content = "Restore Down";
-                }
-            });
-        }
-
-        private void CloseWindow(object? sender, RoutedEventArgs e)
-        {
-            if (VisualRoot is Window window)
-            {
-                window.Close();
-            }
-        }
-
-        private void MaximizeWindow(object? sender, RoutedEventArgs e)
-        {
-            if (VisualRoot is Window window)
-            {
-                if (window.WindowState is WindowState.Normal)
-                {
-                    window.WindowState = WindowState.Maximized;
-                }
-                else
-                {
-                    window.WindowState = WindowState.Normal;
-                }
-            }
-        }
-
-        private void MinimizeWindow(object? sender, RoutedEventArgs e)
-        {
-            if (VisualRoot is Window window)
-            {
-                window.WindowState = WindowState.Minimized;
             }
         }
 
