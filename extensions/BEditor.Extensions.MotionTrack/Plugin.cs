edbc36fc240f79c4449ec838f9c608724cfa9bd5
indigo-san <lugongdao07@gmail.com>
2021-09-05 02:11:37 +0900

    [update] アルゴリズムを選べるようにした 選択範囲を保存するようにした

--- extensions/BEditor.Extensions.MotionTrack/Plugin.cs
+++ extensions/BEditor.Extensions.MotionTrack/Plugin.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.IO;
 
 using BEditor.Data;
 using BEditor.Plugin;
@@ -9,6 +11,8 @@ namespace BEditor.Extensions.MotionTrack
 {
     public sealed class Plugin : PluginObject
     {
+        private SettingRecord? settings;
+
         public Plugin(PluginConfig config) : base(config)
         {
         }
@@ -17,7 +21,11 @@ namespace BEditor.Extensions.MotionTrack
 
         public override string Description => "";
 
-        public override SettingRecord Settings { get; set; } = new();
+        public override SettingRecord Settings
+        {
+            get => settings ??= SettingRecord.LoadFrom<CustomSettings>(Path.Combine(BaseDirectory, "settings.json")) ?? new CustomSettings(Algorithm.KCF);
+            set => (settings = value).Save(Path.Combine(BaseDirectory, "settings.json"));
+        }
 
         public override Guid Id { get; } = Guid.Parse("92B488ED-F31A-4F90-A645-021C9BC99C48");
 
@@ -49,5 +57,16 @@ namespace BEditor.Extensions.MotionTrack
                 })
                 .Register();
         }
+    }
+
+    public record CustomSettings(
+        [property: DisplayName("アルゴリズム")]
+        Algorithm Algorithm) : SettingRecord;
+
+    public enum Algorithm
+    {
+        MIL,
+        KCF,
+        CSRT,
     }
 }
--- extensions/BEditor.Extensions.MotionTrack/Tracker.cs
+++ extensions/BEditor.Extensions.MotionTrack/Tracker.cs
@@ -5,6 +5,7 @@ using BEditor.Data;
 using BEditor.Data.Property;
 using BEditor.Drawing;
 using BEditor.Drawing.Pixel;
+using BEditor.Plugin;
 
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,14 +25,20 @@ namespace BEditor.Extensions.MotionTrack
                 nameof(TargetId),
                 EditingPropertyOptions<ValueProperty>.Create(new ValuePropertyMetadata("ID", 0, Min: 0)).Serialize());
 
+        public static readonly EditingProperty<string?> FileProperty
+            = EditingProperty.Register<string?, Tracker>(
+                nameof(File),
+                EditingPropertyOptions<string>.Create()!.Serialize());
+
+        public static readonly EditingProperty<string?> RoiProperty
+            = EditingProperty.Register<string?, Tracker>(
+                nameof(Roi),
+                EditingPropertyOptions<string>.Create()!.Serialize());
+
         private IDisposable? _disposable;
-
         private OpenCvSharp.Tracker? _tracker;
-
         private Rect _roi;
-
         private bool _isInitialized = false;
-
         private TrackingService _trackingService;
 
         public Tracker()
@@ -44,6 +51,18 @@ namespace BEditor.Extensions.MotionTrack
         public ButtonComponent Crop => GetValue(CropProperty);
 
         public ValueProperty TargetId => GetValue(TargetIdProperty);
+
+        public string? File
+        {
+            get => GetValue(FileProperty);
+            set => SetValue(FileProperty, value);
+        }
+
+        public string? Roi
+        {
+            get => GetValue(RoiProperty);
+            set => SetValue(RoiProperty, value);
+        }
 
         public override unsafe void Apply(EffectApplyArgs args)
         {
@@ -66,10 +85,10 @@ namespace BEditor.Extensions.MotionTrack
                         {
                             _trackingService[(int)TargetId.Value] = new Rectangle(_roi.X, _roi.Y, _roi.Width, _roi.Height);
 
-                            if(args.Type== ApplyType.Edit)
+                            if (args.Type == ApplyType.Edit)
                             {
                                 using var texImage = Image.Rect(_roi.Width, _roi.Height, 2, Colors.White);
-                                using var texture = Graphics.Texture.FromImage(image);
+                                using var texture = Graphics.Texture.FromImage(texImage);
                                 var pos = texture.Transform.Position;
                                 pos.X = _roi.X + _roi.Width / 2 - scene.Width / 2;
                                 pos.Y = -(_roi.Y + _roi.Height / 2 - scene.Height / 2);
@@ -91,38 +110,111 @@ namespace BEditor.Extensions.MotionTrack
             yield return TargetId;
         }
 
-        protected override unsafe void OnLoad()
+        protected override void OnLoad()
         {
             base.OnLoad();
-            _tracker = OpenCvSharp.Tracking.TrackerKCF.Create();
             _trackingService = ServicesLocator.Current.Provider.GetRequiredService<TrackingService>();
-            _disposable = Crop.Subscribe(_ =>
+            _disposable = Crop.Subscribe(async _ =>
             {
                 var scene = this.GetParent<Scene>();
+                var dialog = ServicesLocator.Current.Provider.GetRequiredService<IFileDialogService>();
                 if (scene != null)
                 {
                     _isInitialized = false;
                     using var image = scene.Render(ApplyType.Image);
+                    using var ch3 = image.Convert<BGR24>();
+
+                    unsafe
+                    {
+                        fixed (void* ptr = ch3.Data)
+                        {
+                            using var mat = new Mat(image.Height, image.Width, MatType.CV_8UC3, (IntPtr)ptr, 0);
+
+                            _roi = Cv2.SelectROI(mat);
+
+                            _tracker?.Dispose();
+                            _tracker = Create();
+                            _tracker.Init(mat, _roi);
+
+                            Cv2.DestroyAllWindows();
+                        }
+                    }
+
+                    var record = new SaveFileRecord
+                    {
+                        Filters =
+                        {
+                            new("画像ファイル", new string[]
+                            {
+                                "png",
+                                "jpg",
+                                "jpeg",
+                            })
+                        },
+                    };
+
+                    if (await dialog.ShowSaveFileDialogAsync(record))
+                    {
+                        File = record.FileName;
+                        Roi = FormattableString.Invariant($"{_roi.X},{_roi.Y},{_roi.Width},{_roi.Height}");
+                        image.Save(File);
+                    }
+
+                    _isInitialized = true;
+                }
+            });
+
+            if (System.IO.File.Exists(File) && File != null && Roi != null)
+            {
+                var arr = Roi.Split(',');
+                var x = int.Parse(arr[0]);
+                var y = int.Parse(arr[1]);
+                var width = int.Parse(arr[2]);
+                var height = int.Parse(arr[3]);
+                _roi = new Rect(x, y, width, height);
+                using var image = Image<BGR24>.FromFile(File);
+
+                unsafe
+                {
                     fixed (void* ptr = image.Data)
                     {
-                        using var mat = new Mat(image.Height, image.Width, MatType.CV_8UC4, (IntPtr)ptr, 0);
+                        using var mat = new Mat(image.Height, image.Width, MatType.CV_8UC3, (IntPtr)ptr, 0);
 
-                        _roi = Cv2.SelectROI(mat);
+                        _tracker = Create();
                         _tracker.Init(mat, _roi);
                         _isInitialized = true;
                     }
                 }
-            });
+            }
         }
 
         protected override void OnUnload()
         {
             base.OnUnload();
+
             _tracker?.Dispose();
             _tracker = null;
             _disposable?.Dispose();
             _disposable = null;
             _isInitialized = false;
+        }
+
+        private static OpenCvSharp.Tracker Create()
+        {
+            var plugin = PluginManager.Default.Get<Plugin>();
+
+            if (plugin.Settings is CustomSettings settings)
+            {
+                return settings.Algorithm switch
+                {
+                    Algorithm.MIL => TrackerMIL.Create(),
+                    Algorithm.KCF => OpenCvSharp.Tracking.TrackerKCF.Create(),
+                    Algorithm.CSRT => OpenCvSharp.Tracking.TrackerCSRT.Create(),
+                    _ => OpenCvSharp.Tracking.TrackerKCF.Create(),
+                };
+            }
+
+            return OpenCvSharp.Tracking.TrackerKCF.Create();
         }
     }
 }
