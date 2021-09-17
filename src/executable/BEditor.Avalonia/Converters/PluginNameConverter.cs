dc30997360db28de0912c82349a35be768d15a29
indigo-san <lugongdao07@gmail.com>
2021-09-17 16:57:17 +0900

    [update] 読み込みに失敗したプラグインをアンインストールできるようにした

--- src/executable/BEditor.Avalonia/Converters/PluginNameConverter.cs
+++ src/executable/BEditor.Avalonia/Converters/PluginNameConverter.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 
 using Avalonia.Data.Converters;
 
+using BEditor.Models.ManagePlugins;
 using BEditor.Plugin;
 
 namespace BEditor.Converters
@@ -13,7 +14,7 @@ namespace BEditor.Converters
         {
             if (value is PluginObject plugin)
             {
-                var assembly = plugin.GetType().Assembly;
+                var assembly = plugin is DummyPlugin dummy ? dummy.Assembly : plugin.GetType().Assembly;
                 var name = assembly.GetName();
                 return $"{name.Name} {name.Version}";
             }
