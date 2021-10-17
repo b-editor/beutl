d48772153522c7c4cd2556e7b8e739df0951f642
indigo-san <lugongdao07@gmail.com>
2021-10-17 17:37:42 +0900

    [update] GradientPropertyを追加

---
+++ src/libraries/BEditor.Core/Data/Property/Metadata/GradientPropertyMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BEditor.Drawing;
+
+namespace BEditor.Data.Property
+{
+    public record GradientPropertyMetadata(string Name, Color Color1, Color Color2)
+        : PropertyElementMetadata(Name), IEditingPropertyInitializer<GradientProperty>
+    {
+        public GradientProperty Create()
+        {
+            return new(this);
+        }
+    }
+}
