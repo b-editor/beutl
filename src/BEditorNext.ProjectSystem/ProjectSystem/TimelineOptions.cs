2b8dd5da1d3ac6c8a590d1d27c2670182f85d813
indigo-san <lugongdao07@gmail.com>
2021-12-04 16:50:59 +0900

    TimelineOptionsにデフォルトコンストラクターを実装

--- src/BEditorNext.ProjectSystem/ProjectSystem/TimelineOptions.cs
+++ src/BEditorNext.ProjectSystem/ProjectSystem/TimelineOptions.cs
@@ -1,8 +1,32 @@
-﻿using System.ComponentModel;
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace BEditorNext.ProjectSystem;
 
-public record struct TimelineOptions(float Scale, Vector2 Offset)
+public record struct TimelineOptions
 {
+    private readonly float _scale;
+    private readonly Vector2 _offset;
+
+    public TimelineOptions()
+        : this(1, Vector2.Zero)
+    {
+    }
+
+    public TimelineOptions(float scale, Vector2 offset)
+    {
+        _scale = scale;
+        _offset = offset;
+    }
+
+    public float Scale
+    {
+        get => _scale;
+        init => _scale = Math.Min(value, 1);
+    }
+
+    public Vector2 Offset
+    {
+        get => _offset;
+        init => _offset = Vector2.Max(value, new Vector2(0, 0));
+    }
 }
