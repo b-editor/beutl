6f5d1c111a8d10defee649e900cd97a48417022a
indigo-san <lugongdao07@gmail.com>
2021-12-05 22:37:36 +0900

    プロパティの変更を追跡するクラスを追加

---
+++ tests/BEditorNext.Core.UnitTests/PropertyChangeTrackerTests.cs
@@ -0,0 +1,64 @@
+
+using NUnit.Framework;
+
+namespace BEditorNext.Core.UnitTests;
+
+public class PropertyChangeTrackerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var elm_1 = new TestElement() { String = "" };
+        var elm = new TestElement()
+        {
+            Children =
+            {
+                elm_1
+            }
+        };
+        var array = new Element[] { elm };
+        const string Foo = "Foo";
+        const string Bar = "Bar";
+
+        elm.String = Foo;
+        using (var tracker = new PropertyChangeTracker(array, 0))
+        {
+            elm.String = Bar;
+            elm_1.String = "Depth_1";
+
+            IRecordableCommand command = tracker.ToCommand();
+
+            command.Undo();
+
+            Assert.AreEqual(Foo, elm.String);
+            Assert.AreEqual("Depth_1", elm_1.String);
+
+            command.Redo();
+            Assert.AreEqual(Bar, elm.String);
+            Assert.AreEqual("Depth_1", elm_1.String);
+        }
+    }
+}
+
+public class TestElement : Element
+{
+    public static readonly PropertyDefine<string> StringProperty;
+
+    static TestElement()
+    {
+        StringProperty = RegisterProperty<string, TestElement>("String")
+            .DefaultValue(string.Empty)
+            .NotifyPropertyChanged(true);
+    }
+
+    public string String
+    {
+        get => GetValue(StringProperty);
+        set => SetValue(StringProperty, value);
+    }
+}
