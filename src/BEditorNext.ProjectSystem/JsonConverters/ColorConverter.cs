50c70cdaac102ff60243cc7058273a0371bfd438
indigo-san <lugongdao07@gmail.com>
2021-12-10 17:03:15 +0900

    Color型のJsonConverterを実装

---
+++ src/BEditorNext.ProjectSystem/JsonConverters/ColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using BEditorNext.Media;
+
+namespace BEditorNext.JsonConverters;
+
+internal class ColorConverter : JsonConverter<Color>
+{
+    public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? s = reader.GetString();
+        if (s == null)
+            throw new Exception("Invalid Color.");
+
+        return Color.Parse(s);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
