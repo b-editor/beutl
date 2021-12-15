4441bb75c94122f69d0aa716dfef2258cc44bf85
indigo-san <lugongdao07@gmail.com>
2021-12-15 21:18:00 +0900

    数値の操作をするサービスを追加

---
+++ src/BEditorNext/Services/Editors/FloatEditorService.cs
@@ -0,0 +1,24 @@
+﻿namespace BEditorNext.Services.Editors;
+
+public sealed class FloatEditorService : INumberEditorService<float>
+{
+    public float Clamp(float value, float min, float max)
+    {
+        return Math.Clamp(value, min, max);
+    }
+
+    public float Decrement(float value, int increment)
+    {
+        return value - increment;
+    }
+
+    public float Increment(float value, int increment)
+    {
+        return value + increment;
+    }
+
+    public bool TryParse(string? s, out float result)
+    {
+        return float.TryParse(s, out result);
+    }
+}
---
+++ src/BEditorNext/Services/Editors/INumberEditorService{T}.cs
@@ -0,0 +1,13 @@
+﻿namespace BEditorNext.Services.Editors;
+
+public interface INumberEditorService<T>
+    where T : struct
+{
+    bool TryParse(string? s, out T result);
+
+    T Clamp(T value, T min, T max);
+
+    T Increment(T value, int increment);
+
+    T Decrement(T value, int increment);
+}
