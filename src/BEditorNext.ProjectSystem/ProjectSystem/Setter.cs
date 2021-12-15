700a834f0fedf7f79d02a219d5d843f48a967dfd
indigo-san <lugongdao07@gmail.com>
2021-12-15 20:34:34 +0900

    "System.Text.Json"名前空間のusingを追加

--- src/BEditorNext.ProjectSystem/ProjectSystem/Setter.cs
+++ src/BEditorNext.ProjectSystem/ProjectSystem/Setter.cs
@@ -5,6 +5,7 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace BEditorNext.ProjectSystem;
