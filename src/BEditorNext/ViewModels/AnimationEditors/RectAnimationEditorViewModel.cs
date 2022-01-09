4bc698fc9c7a461649daea5dd5ab6ee9b54a972b
indigo-san <lugongdao07@gmail.com>
2022-01-09 23:17:16 +0900

    RectAnimationEditorを追加

---
+++ src/BEditorNext/ViewModels/AnimationEditors/RectAnimationEditorViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+using BEditorNext.Animation;
+using BEditorNext.Graphics;
+using BEditorNext.ViewModels.Editors;
+
+namespace BEditorNext.ViewModels.AnimationEditors;
+
+public sealed class RectAnimationEditorViewModel : AnimationEditorViewModel<Rect>
+{
+    public RectAnimationEditorViewModel(Animation<Rect> animation, BaseEditorViewModel<Rect> editorViewModel)
+        : base(animation, editorViewModel)
+    {
+    }
+
+    public Rect Maximum => Setter.GetValueOrDefault(PropertyMetaTableKeys.Maximum, new Rect(float.MaxValue, float.MaxValue, float.MaxValue, float.MaxValue));
+
+    public Rect Minimum => Setter.GetValueOrDefault(PropertyMetaTableKeys.Minimum, new Rect(float.MinValue, float.MinValue, float.MinValue, float.MinValue));
+}
---
+++ src/BEditorNext/Views/AnimationEditors/RectAnimationEditor.cs
@@ -0,0 +1,91 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml.MarkupExtensions;
+
+using BEditorNext.Graphics;
+using BEditorNext.ViewModels.AnimationEditors;
+
+namespace BEditorNext.Views.AnimationEditors;
+
+public sealed class RectAnimationEditor : BaseVector4AnimationEditor<Rect>
+{
+    public RectAnimationEditor()
+    {
+        var xres = new DynamicResourceExtension("XString");
+        var yres = new DynamicResourceExtension("YString");
+        var zres = new DynamicResourceExtension("WidthString");
+        var wres = new DynamicResourceExtension("HeightString");
+        prevXText[!TextBlock.TextProperty] = xres;
+        prevYText[!TextBlock.TextProperty] = yres;
+        prevZText[!TextBlock.TextProperty] = zres;
+        prevWText[!TextBlock.TextProperty] = wres;
+
+        nextXText[!TextBlock.TextProperty] = xres;
+        nextYText[!TextBlock.TextProperty] = yres;
+        nextZText[!TextBlock.TextProperty] = zres;
+        nextWText[!TextBlock.TextProperty] = wres;
+
+        prevXTextBox[!TextBox.TextProperty] = new Binding("Animation.Previous.X", BindingMode.OneWay);
+        prevYTextBox[!TextBox.TextProperty] = new Binding("Animation.Previous.Y", BindingMode.OneWay);
+        prevZTextBox[!TextBox.TextProperty] = new Binding("Animation.Previous.Width", BindingMode.OneWay);
+        prevWTextBox[!TextBox.TextProperty] = new Binding("Animation.Previous.Height", BindingMode.OneWay);
+
+        nextXTextBox[!TextBox.TextProperty] = new Binding("Animation.Next.X", BindingMode.OneWay);
+        nextYTextBox[!TextBox.TextProperty] = new Binding("Animation.Next.Y", BindingMode.OneWay);
+        nextZTextBox[!TextBox.TextProperty] = new Binding("Animation.Next.Width", BindingMode.OneWay);
+        nextWTextBox[!TextBox.TextProperty] = new Binding("Animation.Next.Height", BindingMode.OneWay);
+    }
+
+    protected override Rect Clamp(Rect value)
+    {
+        if (DataContext is RectAnimationEditorViewModel vm)
+        {
+            return new Rect(
+                Math.Clamp(value.X, vm.Minimum.X, vm.Maximum.X),
+                Math.Clamp(value.Y, vm.Minimum.Y, vm.Maximum.Y),
+                Math.Clamp(value.Width, vm.Minimum.Width, vm.Maximum.Width),
+                Math.Clamp(value.Height, vm.Minimum.Height, vm.Maximum.Height));
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    protected override Rect IncrementX(Rect value, int increment)
+    {
+        return value.WithX(value.X + increment);
+    }
+
+    protected override Rect IncrementY(Rect value, int increment)
+    {
+        return value.WithY(value.Y + increment);
+    }
+
+    protected override Rect IncrementZ(Rect value, int increment)
+    {
+        return value.WithWidth(value.Width + increment);
+    }
+
+    protected override Rect IncrementW(Rect value, int increment)
+    {
+        return value.WithHeight(value.Height + increment);
+    }
+
+    protected override bool TryParse(string? x, string? y, string? z, string? w, out Rect value)
+    {
+        if (float.TryParse(x, out float xi) &&
+            float.TryParse(y, out float yi) &&
+            float.TryParse(z, out float zi) &&
+            float.TryParse(w, out float wi))
+        {
+            value = new Rect(xi, yi, zi, wi);
+            return true;
+        }
+        else
+        {
+            value = default;
+            return false;
+        }
+    }
+}
