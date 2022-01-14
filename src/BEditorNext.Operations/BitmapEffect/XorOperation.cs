ffc71a025a9d9eb05355991c022ffd907fa17e0c
indigo-san <lugongdao07@gmail.com>
2022-01-14 18:23:29 +0900

    BitmapEffectを一旦削除

--- src/BEditorNext.Operations/BitmapEffect/XorOperation.cs
+++
@@ -1,8 +0,0 @@
-﻿using BEditorNext.Graphics.Effects;
-
-namespace BEditorNext.Operations.BitmapEffect;
-
-public sealed class XorOperation : BitmapEffectOperation<Xor>
-{
-    public override Xor Effect { get; } = new();
-}
