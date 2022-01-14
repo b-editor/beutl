50d0becceb852091483c3f59fd9d6337927ed59e
indigo-san <lugongdao07@gmail.com>
2022-01-14 18:24:12 +0900

    ImageFilterを追加

---
+++ src/BEditorNext.Operations/Filters/ImageFilterOperation.cs
@@ -0,0 +1,23 @@
+﻿using BEditorNext.Graphics;
+using BEditorNext.ProjectSystem;
+using BEditorNext.Rendering;
+
+namespace BEditorNext.Operations.Filters;
+
+public abstract class ImageFilterOperation<T> : LayerOperation
+    where T : Graphics.Filters.ImageFilter
+{
+    public abstract T Filter { get; }
+
+    public override void Render(in OperationRenderArgs args)
+    {
+        for (int i = 0; i < args.List.Count; i++)
+        {
+            IRenderable item = args.List[i];
+            if (item is IDrawable bmp)
+            {
+                bmp.Filters.Add(Filter);
+            }
+        }
+    }
+}
