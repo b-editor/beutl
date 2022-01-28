081873d4c13dd8707a9a64a7aba0ee1c7d56330c
indigo-san <lugongdao07@gmail.com>
2022-01-28 13:33:23 +0900

    TextFormattingPlaygroundを追加

---
+++ tests/TextFormattingPlayground/MainWindow.axaml.cs
@@ -0,0 +1,78 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+
+using BeUtl.Graphics;
+using BeUtl.Media.TextFormatting;
+
+using Canvas = BeUtl.Graphics.Canvas;
+
+namespace TextFormattingPlayground;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        textBox.GetObservable(TextBox.TextProperty).Subscribe(TextChanged);
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private async void TextChanged(string obj)
+    {
+        try
+        {
+            await Task.Delay(500);
+            Draw(FormattedText.Parse(obj ?? string.Empty, FormattedTextInfo.Default with
+            {
+                Size = 70
+            }));
+        }
+        catch
+        {
+
+        }
+    }
+
+    private unsafe void Draw(FormattedText text)
+    {
+        text.Measure(BeUtl.Graphics.Size.Empty);
+        var bounds = text.Bounds;
+        var width = (int)bounds.Width;
+        var height = (int)bounds.Height;
+
+        if (width <= 0 || height <= 0)
+            return;
+
+        using var canvas = new Canvas(width, height);
+        canvas.Clear(BeUtl.Media.Colors.Black);
+        text.Draw(canvas);
+
+        using var bmp = canvas.GetBitmap();
+
+        if (image.Source is WriteableBitmap old)
+        {
+            image.Source = null;
+            old.Dispose();
+            old.PlatformImpl.Item?.Dispose();
+        }
+
+        var wbmp = new WriteableBitmap(
+            new PixelSize(canvas.Size.Width, canvas.Size.Height),
+            Avalonia.Skia.SkiaPlatform.DefaultDpi,
+            Avalonia.Platform.PixelFormat.Bgra8888,
+            Avalonia.Platform.AlphaFormat.Premul);
+
+        using (var buf = wbmp.Lock())
+        {
+            Buffer.MemoryCopy((void*)bmp.Data, (void*)buf.Address, bmp.ByteCount, bmp.ByteCount);
+        }
+
+        image.Source = wbmp;
+        image.InvalidateVisual();
+    }
+}
