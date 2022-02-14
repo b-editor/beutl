258a403cdefdf0ce51a1264b915854d542180ebb
indigo-san <lugongdao07@gmail.com>
2022-02-14 14:41:23 +0900

    DirectoryTreeViewを追加

---
+++ tests/DirectoryViewTest/MainWindow.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+using BeUtl.Controls;
+
+using FluentAvalonia.UI.Controls;
+
+namespace DirectoryViewTest;
+public partial class MainWindow : CoreWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Content = new DirectoryTreeView(new FileSystemWatcher("D:\\source")
+        {
+            EnableRaisingEvents = true,
+            IncludeSubdirectories = true
+        });
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
