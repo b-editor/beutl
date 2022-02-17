e78d77902e096d97f1690a0f25913ce9ded525e9
indigo-san <lugongdao07@gmail.com>
2022-02-17 14:23:29 +0900

    日本語の言語リソースを追加

---
+++ src/BeUtl.Language/LocalizeService.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace BeUtl.Language;
+
+public sealed class LocalizeService
+{
+    public static readonly LocalizeService Instance = new();
+    private readonly string[] _supported =
+    {
+        "en-US",
+        "ja-JP",
+    };
+
+    public bool IsSupportedCulture(CultureInfo ci)
+    {
+        return _supported.Contains(ci.Name);
+    }
+
+    public Uri GetUri(CultureInfo ci)
+    {
+        if (!IsSupportedCulture(ci)) throw new InvalidOperationException();
+        return new Uri($"avares://BeUtl.Language/{ci.Name}/CommonResources.axaml");
+    }
+
+    public IEnumerable<CultureInfo> SupportedCultures()
+    {
+        return _supported.Select(n => CultureInfo.GetCultureInfo(n));
+    }
+}
