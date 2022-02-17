04f713ea1ebe605d22063c338d22bc67f58d39ca
indigo-san <lugongdao07@gmail.com>
2022-02-18 07:49:47 +0900

    Config系のクラスをファイルごとに分割した

---
+++ src/BeUtl.Configuration/GlobalConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json.Nodes;
+
+namespace BeUtl.Configuration;
+
+public sealed class GlobalConfiguration
+{
+    public static readonly GlobalConfiguration Instance = new();
+    private JsonObject _json = new();
+
+    public static string DefaultFilePath
+    {
+        get
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "beutl", "settings.json");
+            }
+            else
+            {
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config", "beutl", "settings.json");
+            }
+        }
+    }
+
+    public GraphicsConfig GraphicsConfig { get; } = new();
+
+    public FontConfig FontConfig { get; } = new();
+
+    public ViewConfig ViewConfig { get; } = new();
+
+    public void Save(string file)
+    {
+        string dir = Path.GetDirectoryName(file)!;
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        _json["font"] = FontConfig.ToJson();
+        _json["view"] = ViewConfig.ToJson();
+
+        _json.JsonSave(file);
+    }
+
+    public void Restore(string file)
+    {
+        if (JsonHelper.JsonRestore(file) is JsonObject json)
+        {
+            FontConfig.FromJson(json["font"]!);
+            ViewConfig.FromJson(json["view"]!);
+
+            _json = json;
+        }
+    }
+}
