072b7a26600ca9f31641224798cd28bfe13dcc48
indigo-san <lugongdao07@gmail.com>
2022-04-09 10:30:52 +0900

    SceneEditorExtemsion.csを追加

---
+++ src/BeUtl.Framework/SceneEditorExtension.cs
@@ -0,0 +1,20 @@
+﻿using BeUtl.ProjectSystem;
+
+namespace BeUtl.Framework;
+
+public abstract class SceneEditorExtension : ViewExtension
+{
+    public enum TabPlacement
+    {
+        Bottom,
+        Right
+    }
+
+    public abstract TabPlacement Placement { get; }
+
+    public abstract bool IsClosable { get; }
+
+    public abstract ResourceReference<string> Header { get; }
+
+    public abstract IEditor CreateContent(Scene scene);
+}
