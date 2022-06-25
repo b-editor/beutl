627694b5794de4df3c901e6e3d53ee7689b46aea
indigo-san <lugongdao07@gmail.com>
2022-06-25 21:13:34 +0900

    FontFamilyのプロパティエディタを修正

--- src/BeUtl/ViewModels/Editors/FontFamilyEditorViewModel.cs
+++ src/BeUtl/ViewModels/Editors/FontFamilyEditorViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BeUtl.Media;
 using BeUtl.Services.Editors.Wrappers;
 
+using Reactive.Bindings;
+
 namespace BeUtl.ViewModels.Editors;
 
 public sealed class FontFamilyEditorViewModel : BaseEditorViewModel<FontFamily>
@@ -8,5 +10,10 @@ public sealed class FontFamilyEditorViewModel : BaseEditorViewModel<FontFamily>
     public FontFamilyEditorViewModel(IWrappedProperty<FontFamily> property)
         : base(property)
     {
+        Value = property.GetObservable()
+            .ToReadOnlyReactivePropertySlim()
+            .DisposeWith(Disposables);
     }
+
+    public ReadOnlyReactivePropertySlim<FontFamily> Value { get; }
 }
