c1a8a6579d7f44430b1e2227429f0fdba1c933e5
indigo-san <lugongdao07@gmail.com>
2022-06-29 19:39:38 +0900

    Brush関係を修正

--- src/BeUtl.Graphics/Media/IBrush.cs
+++ src/BeUtl.Graphics/Media/IBrush.cs
@@ -1,10 +1,14 @@
-﻿using BeUtl.Styling;
+﻿using System.Text.Json.Serialization;
+
+using BeUtl.Converters;
+using BeUtl.Styling;
 
 namespace BeUtl.Media;
 
 /// <summary>
 /// Describes how an area is painted.
 /// </summary>
+[JsonConverter(typeof(BrushJsonConverter))]
 public interface IBrush
 {
     /// <summary>
--- src/BeUtl.Operations/Shapes/RoundedRectOperation.cs
+++ src/BeUtl.Operations/Shapes/RoundedRectOperation.cs
@@ -62,7 +62,17 @@ public sealed class RoundedRectOperation : DrawableOperation
     public Color Color
     {
         get => _drawable.Foreground?.TryGetColorOrDefault(Colors.Transparent) ?? Colors.Transparent;
-        set => _drawable.Foreground = value.ToImmutableBrush();
+        set
+        {
+            if (_drawable.Foreground is SolidColorBrush solidColorBrush)
+            {
+                solidColorBrush.Color = value;
+            }
+            else
+            {
+                _drawable.Foreground = new SolidColorBrush(value);
+            }
+        }
     }
 
     public CornerRadius CornerRadius
