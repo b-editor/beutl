5a7b04d44ed8e33f760b65e92d8da65a0821a94d
indigo-san <lugongdao07@gmail.com>
2022-07-02 19:26:44 +0900

    StyleをJSONに保存するクラスを追加

---
+++ src/BeUtl.Graphics/Styling/StyleSerializer.cs
@@ -0,0 +1,235 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+using BeUtl.Animation;
+
+namespace BeUtl.Styling;
+
+public static class StyleSerializer
+{
+    public static JsonNode ToJson(this IStyle style)
+    {
+        var styleJson = new JsonObject
+        {
+            ["target"] = TypeFormat.ToString(style.TargetType)
+        };
+
+        var setters = new JsonArray();
+        foreach (ISetter? item in style.Setters)
+        {
+            setters.Add(item.ToJson(style.TargetType));
+        }
+        styleJson["setters"] = setters;
+
+        return styleJson;
+    }
+
+    public static JsonNode ToJson(this ISetter setter, Type targetType)
+    {
+        var json = new JsonObject();
+
+        if (targetType.IsAssignableTo(setter.Property.OwnerType))
+        {
+            json["property"] = setter.Property.Name;
+        }
+        else
+        {
+            json["property"] = $"{setter.Property.Name}:{TypeFormat.ToString(setter.Property.OwnerType)}";
+        }
+
+        if (setter.Value != null)
+        {
+            json["value"] = SerializeValue(setter.Property.PropertyType, setter.Value);
+        }
+
+        if (setter.Animations.Any())
+        {
+            var animations = new JsonArray();
+
+            foreach (IAnimation item in setter.Animations)
+            {
+                JsonNode anmNode = new JsonObject();
+                item.WriteToJson(ref anmNode);
+                animations.Add(anmNode);
+            }
+
+            json["animations"] = animations;
+        }
+
+        return json;
+    }
+
+    public static JsonNode? SerializeValue(Type type, object value)
+    {
+        if (value is IJsonSerializable serializable)
+        {
+            JsonNode jsonNode = new JsonObject();
+            serializable.WriteToJson(ref jsonNode);
+
+            Type? objType = value.GetType();
+            if (objType != type && jsonNode is JsonObject)
+            {
+                jsonNode["@type"] = TypeFormat.ToString(objType);
+            }
+
+            return jsonNode;
+        }
+        else
+        {
+            return JsonSerializer.SerializeToNode(value, type, JsonHelper.SerializerOptions);
+        }
+    }
+
+    public static Style? ToStyle(this JsonObject json)
+    {
+        if (json.TryGetPropertyValue("target", out JsonNode? targetNode)
+            && targetNode is JsonValue targetValue
+            && targetValue.TryGetValue(out string? targetStr)
+            && TypeFormat.ToType(targetStr) is Type targetType)
+        {
+            var style = new Style
+            {
+                TargetType = targetType
+            };
+
+            if (json.TryGetPropertyValue("setters", out JsonNode? settersNode)
+                && settersNode is JsonArray settersArray)
+            {
+                foreach (JsonNode? item in settersArray)
+                {
+                    if (item != null && item.ToSetter(targetType) is ISetter setter)
+                    {
+                        style.Setters.Add(setter);
+                    }
+                }
+            }
+
+            return style;
+        }
+
+        return null;
+    }
+
+    public static ISetter? ToSetter(this JsonNode json, Type targetType)
+    {
+        if (json is JsonObject jobj)
+        {
+            if (jobj.TryGetPropertyValue("property", out JsonNode? propertyNode)
+                && propertyNode is JsonValue propertyValue
+                && propertyValue.TryGetValue(out string? propertyStr))
+            {
+                int separator = propertyStr.IndexOf(":");
+                CoreProperty? property = null;
+                if (separator < 0)
+                {
+                    property = PropertyRegistry.FindRegistered(targetType, propertyStr);
+                }
+                else
+                {
+                    string name = propertyStr[..separator];
+                    var type = TypeFormat.ToType(propertyStr[separator..]);
+                    if (type != null)
+                    {
+                        property = PropertyRegistry.FindRegistered(type, name);
+                    }
+                }
+
+                if (property == null)
+                    return null;
+
+                var helper = (IGenericHelper)typeof(GenericHelper<>)
+                    .MakeGenericType(property.PropertyType)
+                    .GetField("Instance")!
+                    .GetValue(null)!;
+
+                object? value = null;
+                if (jobj.TryGetPropertyValue("value", out JsonNode? valueNode))
+                {
+                    value = DeserializeValue(property.PropertyType, valueNode!);
+                }
+
+                var animations = new List<BaseAnimation>();
+
+                if (jobj.TryGetPropertyValue("animations", out JsonNode? animationsNode)
+                    && animationsNode is JsonArray animationsArray)
+                {
+                    if (animations.Capacity < animationsArray.Count)
+                    {
+                        animations.Capacity = animationsArray.Count;
+                    }
+                    foreach (JsonNode? item in animationsArray)
+                    {
+                        if (item is JsonObject animationObj)
+                        {
+                            animations.Add(helper.DeserializeAnimation(animationObj));
+                        }
+                    }
+                }
+
+                return helper.InitializeSetter(property, value, animations);
+            }
+        }
+
+        return null;
+    }
+
+    public static object? DeserializeValue(Type type, JsonNode jsonNode)
+    {
+        if (jsonNode is JsonObject jsonObject
+            && jsonObject.TryGetPropertyValue("@type", out JsonNode? atTypeNode)
+            && atTypeNode is JsonValue atTypeValue
+            && atTypeValue.TryGetValue(out string? atTypeStr)
+            && TypeFormat.ToType(atTypeStr) is Type realType
+            && realType.IsAssignableTo(type)
+            && realType.IsAssignableTo(typeof(IJsonSerializable)))
+        {
+            var sobj = (IJsonSerializable?)Activator.CreateInstance(realType);
+            if (sobj != null)
+            {
+                sobj.ReadFromJson(jsonNode!);
+                return sobj;
+            }
+        }
+        else if (type.IsAssignableTo(typeof(IJsonSerializable)))
+        {
+            var sobj = (IJsonSerializable?)Activator.CreateInstance(type);
+            if (sobj != null)
+            {
+                sobj.ReadFromJson(jsonNode!);
+                return sobj;
+            }
+        }
+        else
+        {
+            return JsonSerializer.Deserialize(jsonNode, type, JsonHelper.SerializerOptions);
+        }
+
+        return type.IsValueType ? Activator.CreateInstance(type) : null;
+    }
+
+    private interface IGenericHelper
+    {
+        ISetter InitializeSetter(CoreProperty property, object? value, IEnumerable<BaseAnimation> animations);
+
+        BaseAnimation DeserializeAnimation(JsonObject json);
+    }
+
+    private sealed class GenericHelper<T> : IGenericHelper
+    {
+        public static readonly GenericHelper<T> Instance = new();
+
+        public ISetter InitializeSetter(CoreProperty property, object? value, IEnumerable<BaseAnimation> animations)
+        {
+            var setter = new Setter<T>((CoreProperty<T>)property, (T?)value);
+            setter.Animations.AddRange(animations.OfType<Animation<T>>());
+            return setter;
+        }
+
+        public BaseAnimation DeserializeAnimation(JsonObject json)
+        {
+            var anm = new Animation<T>();
+            anm.ReadFromJson(json);
+            return anm;
+        }
+    }
+}
--- src/BeUtl.Graphics/Styling/Styleable.cs
+++ src/BeUtl.Graphics/Styling/Styleable.cs
@@ -89,154 +89,6 @@ public abstract class Styleable : Element, IStyleable
         _styleInstance = instance;
     }
 
-    private static Style? ReadStyleFromJsonObject(JsonObject json)
-    {
-        if (json.TryGetPropertyValue("target", out JsonNode? targetNode)
-            && targetNode is JsonValue targetValue
-            && targetValue.TryGetValue(out string? targetStr)
-            && TypeFormat.ToType(targetStr) is Type targetType)
-        {
-            var style = new Style
-            {
-                TargetType = targetType
-            };
-
-            if (json.TryGetPropertyValue("setters", out JsonNode? settersNode)
-                && settersNode is JsonArray settersArray)
-            {
-                foreach (JsonNode? item in settersArray)
-                {
-                    if (item != null && ReadSetterFromJson(item, targetType) is ISetter setter)
-                    {
-                        style.Setters.Add(setter);
-                    }
-                }
-            }
-
-            return style;
-        }
-
-        return null;
-    }
-
-    private static ISetter? ReadSetterFromJson(JsonNode json, Type targetType)
-    {
-        if (json is JsonObject jobj)
-        {
-            if (jobj.TryGetPropertyValue("property", out JsonNode? propertyNode)
-                && propertyNode is JsonValue propertyValue
-                && propertyValue.TryGetValue(out string? propertyStr))
-            {
-                int separator = propertyStr.IndexOf(":");
-                CoreProperty? property = null;
-                if (separator < 0)
-                {
-                    property = PropertyRegistry.FindRegistered(targetType, propertyStr);
-                }
-                else
-                {
-                    string name = propertyStr[..separator];
-                    var type = TypeFormat.ToType(propertyStr[separator..]);
-                    if (type != null)
-                    {
-                        property = PropertyRegistry.FindRegistered(type, name);
-                    }
-                }
-
-                if (property == null)
-                    return null;
-
-                var helper = (IGenericHelper)typeof(GenericHelper<>)
-                    .MakeGenericType(property.PropertyType)
-                    .GetField("Instance")!
-                    .GetValue(null)!;
-
-                object? value = null;
-                if (jobj.TryGetPropertyValue("value", out JsonNode? valueNode))
-                {
-                    value = DeserializeValue(property.PropertyType, valueNode!);
-                }
-
-                var animations = new List<BaseAnimation>();
-
-                if (jobj.TryGetPropertyValue("animations", out JsonNode? animationsNode)
-                    && animationsNode is JsonArray animationsArray)
-                {
-                    if (animations.Capacity < animationsArray.Count)
-                    {
-                        animations.Capacity = animationsArray.Count;
-                    }
-                    foreach (JsonNode? item in animationsArray)
-                    {
-                        if (item is JsonObject animationObj)
-                        {
-                            animations.Add(helper.DeserializeAnimation(animationObj));
-                        }
-                    }
-                }
-
-                return helper.InitializeSetter(property, value, animations);
-            }
-        }
-
-        return null;
-    }
-
-    private static object? DeserializeValue(Type type, JsonNode jsonNode)
-    {
-        if (jsonNode is JsonObject jsonObject
-            && jsonObject.TryGetPropertyValue("@type", out JsonNode? atTypeNode)
-            && atTypeNode is JsonValue atTypeValue
-            && atTypeValue.TryGetValue(out string? atTypeStr)
-            && TypeFormat.ToType(atTypeStr) is Type realType
-            && realType.IsAssignableTo(type)
-            && realType.IsAssignableTo(typeof(IJsonSerializable)))
-        {
-            var sobj = (IJsonSerializable?)Activator.CreateInstance(realType);
-            if (sobj != null)
-            {
-                sobj.ReadFromJson(jsonNode!);
-                return sobj;
-            }
-        }
-        else if (type.IsAssignableTo(typeof(IJsonSerializable)))
-        {
-            var sobj = (IJsonSerializable?)Activator.CreateInstance(type);
-            if (sobj != null)
-            {
-                sobj.ReadFromJson(jsonNode!);
-                return sobj;
-            }
-        }
-        else
-        {
-            return JsonSerializer.Deserialize(jsonNode, type, JsonHelper.SerializerOptions);
-        }
-
-        return type.IsValueType ? Activator.CreateInstance(type) : null;
-    }
-
-    private static JsonNode? SerializeValue(Type type, object value)
-    {
-        if (value is IJsonSerializable child)
-        {
-            JsonNode jsonNode = new JsonObject();
-            child.WriteToJson(ref jsonNode);
-
-            var objType = value.GetType();
-            if (objType != type && jsonNode is JsonObject)
-            {
-                jsonNode["@type"] = TypeFormat.ToString(objType);
-            }
-
-            return jsonNode;
-        }
-        else
-        {
-            return JsonSerializer.SerializeToNode(value, type, JsonHelper.SerializerOptions);
-        }
-    }
-
     public override void ReadFromJson(JsonNode json)
     {
         base.ReadFromJson(json);
@@ -254,48 +106,13 @@ public abstract class Styleable : Element, IStyleable
                 foreach (JsonNode? styleNode in stylesArray)
                 {
                     if (styleNode is JsonObject styleObject
-                        && ReadStyleFromJsonObject(styleObject) is Style style)
+                        && styleObject.ToStyle() is Style style)
                     {
                         Styles.Add(style);
                     }
                 }
             }
         }
-    }
-
-    private static JsonNode WriteSetterToJson(ISetter setter, Type targetType)
-    {
-        var json = new JsonObject();
-
-        if (targetType.IsAssignableTo(setter.Property.OwnerType))
-        {
-            json["property"] = setter.Property.Name;
-        }
-        else
-        {
-            json["property"] = $"{setter.Property.Name}:{TypeFormat.ToString(setter.Property.OwnerType)}";
-        }
-
-        if (setter.Value != null)
-        {
-            json["value"] = SerializeValue(setter.Property.PropertyType, setter.Value);
-        }
-
-        if (setter.Animations.Any())
-        {
-            var animations = new JsonArray();
-
-            foreach (var item in setter.Animations)
-            {
-                JsonNode anmNode = new JsonObject();
-                item.WriteToJson(ref anmNode);
-                animations.Add(anmNode);
-            }
-
-            json["animations"] = animations;
-        }
-
-        return json;
     }
 
     public override void WriteToJson(ref JsonNode json)
@@ -309,19 +126,7 @@ public abstract class Styleable : Element, IStyleable
 
                 foreach (IStyle style in Styles.AsSpan())
                 {
-                    var styleJson = new JsonObject
-                    {
-                        ["target"] = TypeFormat.ToString(style.TargetType)
-                    };
-
-                    var setters = new JsonArray();
-                    foreach (ISetter? item in style.Setters)
-                    {
-                        setters.Add(WriteSetterToJson(item, style.TargetType));
-                    }
-                    styleJson["setters"] = setters;
-
-                    styles.Add(styleJson);
+                    styles.Add(style.ToJson());
                 }
 
                 jobject["styles"] = styles;
