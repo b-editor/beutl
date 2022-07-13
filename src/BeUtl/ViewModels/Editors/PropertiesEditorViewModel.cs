ddcbb25012a67e08af45844469e0628d656e23a3
indigo-san <lugongdao07@gmail.com>
2022-07-13 17:30:38 +0900

    PropertiesEditorViewModelをStyleableに対応した

--- src/BeUtl/ViewModels/Editors/PropertiesEditorViewModel.cs
+++ src/BeUtl/ViewModels/Editors/PropertiesEditorViewModel.cs
@@ -2,6 +2,7 @@
 
 using BeUtl.Services;
 using BeUtl.Services.Editors.Wrappers;
+using BeUtl.Styling;
 
 namespace BeUtl.ViewModels.Editors;
 
@@ -10,6 +11,30 @@ public sealed class PropertiesEditorViewModel : IDisposable
     public PropertiesEditorViewModel(ICoreObject obj, Predicate<CorePropertyMetadata>? predicate = null)
     {
         Target = obj;
+        if (obj is Styleable styleable)
+        {
+            InitializeStyleable(styleable, predicate);
+        }
+        else
+        {
+            InitializeCoreObject(obj, predicate);
+        }
+    }
+
+    public ICoreObject Target { get; }
+
+    public CoreList<BaseEditorViewModel> Properties { get; } = new();
+
+    public void Dispose()
+    {
+        foreach (BaseEditorViewModel item in Properties.AsSpan())
+        {
+            item.Dispose();
+        }
+    }
+
+    private void InitializeCoreObject(ICoreObject obj, Predicate<CorePropertyMetadata>? predicate = null)
+    {
         Type objType = obj.GetType();
         Type wrapperType = typeof(CorePropertyWrapper<>);
 
@@ -37,15 +62,59 @@ public sealed class PropertiesEditorViewModel : IDisposable
         }
     }
 
-    public ICoreObject Target { get; }
-
-    public CoreList<BaseEditorViewModel> Properties { get; } = new();
-
-    public void Dispose()
+    private void InitializeStyleable(Styleable styleable, Predicate<CorePropertyMetadata>? predicate = null)
     {
-        foreach (BaseEditorViewModel item in Properties.AsSpan())
+        predicate ??= x => x.PropertyFlags.HasFlag(PropertyFlags.Styleable);
+        Type objType = styleable.GetType();
+        IReadOnlyList<CoreProperty> props = PropertyRegistry.GetRegistered(objType);
+
+        var style = styleable.Styles.FirstOrDefault();
+        if (style == null)
         {
-            item.Dispose();
+            var style1 = new Style()
+            {
+                TargetType = objType
+            };
+
+            for (int i = 0; i < props.Count; i++)
+            {
+                CoreProperty item = props[i];
+                CorePropertyMetadata metadata = item.GetMetadata<CorePropertyMetadata>(objType);
+                Type setterType = typeof(Setter<>).MakeGenericType(item.PropertyType);
+                if (predicate.Invoke(metadata)
+                    && Activator.CreateInstance(setterType, item, styleable.GetValue(item)) is ISetter setter)
+                {
+                    style1.Setters.Add(setter);
+                }
+            }
+
+            styleable.Styles.Add(style1);
+            style = style1;
+        }
+
+        if (Properties.Capacity < style.Setters.Count)
+        {
+            Properties.Capacity = style.Setters.Count;
+        }
+
+        Type wrapperType = typeof(StylingSetterWrapper<>);
+        for (int i = 0; i < style.Setters.Count; i++)
+        {
+            ISetter item = style.Setters[i];
+            CoreProperty property = item.Property;
+
+            if (predicate?.Invoke(property.GetMetadata<CorePropertyMetadata>(objType)) ?? true)
+            {
+                Type wrapperGType = wrapperType.MakeGenericType(property.PropertyType);
+                var wrapper = (IWrappedProperty)Activator.CreateInstance(wrapperGType, item)!;
+
+                BaseEditorViewModel? itemViewModel = PropertyEditorService.CreateEditorViewModel(wrapper);
+
+                if (itemViewModel != null)
+                {
+                    Properties.Add(itemViewModel);
+                }
+            }
         }
     }
 }
