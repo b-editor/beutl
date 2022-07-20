6768849692ce9a2e3bbb66fc366fa86fc2dfef5c
indigo-san <lugongdao07@gmail.com>
2022-07-21 07:40:59 +0900

    新しいTextFormatting APIのベンチマークを追加

--- tests/BeUtl.Benchmarks/FormattedTextBench.cs
+++ tests/BeUtl.Benchmarks/FormattedTextBench.cs
@@ -1,27 +1,30 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Runtime.InteropServices;
+
+using BenchmarkDotNet.Attributes;
+
+using BeUtl.Media.TextFormatting;
 
 [MemoryDiagnoser]
 public class FormattedTextBench
 {
-    private const string Str = @"
-<b>吾輩</b><size=70>は</size><#ff0000>猫</#><size=70>である。</size>
+    private const string Str = @"<b>吾輩</b><size=70>は</size><#ff0000>猫</#><size=70>である。</size>
 <i>名前</i><size=70>はまだ</size>無<size=70>い。</cspace>
 
 <font='Roboto'>Roboto</font>
-<noparse><font='Noto Sans JP'><bold>Noto Sans</font></bold></noparse>
-";
+<noparse><font='Noto Sans JP'><bold>Noto Sans</font></bold></noparse>";
     [Benchmark]
     public void Old()
     {
-        var tokenizer = new BeUtl.Benchmarks.Media.TextFormatting.FormattedTextTokenizer(Str);
-        var tokens = tokenizer.Tokenize();
+        var parser = new FormattedTextParser(Str);
+        var lines = parser.ToLines(FormattedTextInfo.Default);
     }
 
     [Benchmark]
     public void New()
     {
-        var parser = new BeUtl.Media.TextFormatting.FormattedTextTokenizer(Str);
-        parser.Tokenize();
-        _ = parser.Result;
+        var tokenizer = new FormattedTextTokenizer(Str);
+        tokenizer.Tokenize();
+
+        var elements = FormattedTextParser.ToElements(FormattedTextInfo.Default, CollectionsMarshal.AsSpan(tokenizer.Result));
     }
 }
