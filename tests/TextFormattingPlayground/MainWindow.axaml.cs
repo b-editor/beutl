b3d4459c9e721804e69092554cf1b9e906f94688
indigo-san <lugongdao07@gmail.com>
2022-08-04 22:37:28 +0900

    TextFormattingPlaygroundを最新のAPIを使うようにした

--- tests/TextFormattingPlayground/MainWindow.axaml.cs
+++ tests/TextFormattingPlayground/MainWindow.axaml.cs
@@ -1,11 +1,6 @@
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 using Avalonia.Media.Imaging;
-
-using BeUtl.Graphics;
-using BeUtl.Media.TextFormatting;
-using BeUtl.Media.TextFormatting.Compat;
 
 using Canvas = BeUtl.Graphics.Canvas;
 
@@ -13,9 +8,15 @@ namespace TextFormattingPlayground;
 
 public partial class MainWindow : Window
 {
+    private readonly BeUtl.Graphics.Shapes.TextBlock _text;
+
     public MainWindow()
     {
         InitializeComponent();
+        _text = new BeUtl.Graphics.Shapes.TextBlock()
+        {
+            Size = 70
+        };
 
         textBox.GetObservable(TextBox.TextProperty).Subscribe(TextChanged);
 #if DEBUG
@@ -28,10 +29,8 @@ public partial class MainWindow : Window
         try
         {
             await Task.Delay(500);
-            Draw(BeUtl.Media.TextFormatting.Compat.FormattedText.Parse(obj ?? string.Empty, FormattedTextInfo.Default with
-            {
-                Size = 70
-            }));
+            _text.Text = obj ?? string.Empty;
+            Draw();
         }
         catch
         {
@@ -39,10 +38,10 @@ public partial class MainWindow : Window
         }
     }
 
-    private unsafe void Draw(BeUtl.Media.TextFormatting.Compat.FormattedText text)
+    private unsafe void Draw()
     {
-        text.Measure(BeUtl.Graphics.Size.Empty);
-        var bounds = text.Bounds;
+        _text.Measure(BeUtl.Graphics.Size.Infinity);
+        var bounds = _text.Bounds;
         var width = (int)bounds.Width;
         var height = (int)bounds.Height;
 
@@ -51,7 +50,7 @@ public partial class MainWindow : Window
 
         using var canvas = new Canvas(width, height);
         canvas.Clear(BeUtl.Media.Colors.Black);
-        text.Draw(canvas);
+        _text.Draw(canvas);
 
         using var bmp = canvas.GetBitmap();
 
