531388e10e34f3c7e95cdb30fd196c926fd92436
indigo-san <lugongdao07@gmail.com>
2022-08-07 09:00:24 +0900

    MainViewのNavigationViewのアイコンを修正

--- src/BeUtl.Controls/NavItemHelper.cs
+++ src/BeUtl.Controls/NavItemHelper.cs
@@ -17,6 +17,8 @@ public class NavItemHelper : Behavior<NavigationViewItem>
     public static readonly StyledProperty<Geometry> FilledIconProperty
         = AvaloniaProperty.Register<NavItemHelper, Geometry>("FilledIcon");
     private IDisposable _disposable;
+    private FAPathIcon _regular;
+    private FAPathIcon _filled;
 
     public Geometry RegularIcon
     {
@@ -33,6 +35,14 @@ public class NavItemHelper : Behavior<NavigationViewItem>
     protected override void OnAttached()
     {
         base.OnAttached();
+        _regular = new FAPathIcon()
+        {
+            Data = RegularIcon
+        };
+        _filled = new FAPathIcon()
+        {
+            Data = FilledIcon
+        };
         _disposable = AssociatedObject.GetPropertyChangedObservable(ListBoxItem.IsSelectedProperty)
             .Subscribe(e => SelectionChanged((NavigationViewItem)e.Sender));
 
@@ -42,21 +52,37 @@ public class NavItemHelper : Behavior<NavigationViewItem>
     protected override void OnDetaching()
     {
         base.OnDetaching();
+        _regular = null;
+        _filled = null;
         _disposable?.Dispose();
+    }
+
+    protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property.Name is nameof(RegularIcon) or nameof(FilledIcon))
+        {
+            if (_regular != null)
+            {
+                _regular.Data = RegularIcon;
+            }
+
+            if (_filled != null)
+            {
+                _filled.Data = FilledIcon;
+            }
+        }
     }
 
     private void SelectionChanged(NavigationViewItem sender)
     {
-        FAPathIcon icon = sender.Icon as FAPathIcon ?? new FAPathIcon();
         if (sender.IsSelected)
         {
-            icon.Data = FilledIcon ?? RegularIcon;
+            sender.Icon = _filled;
         }
         else
         {
-            icon.Data = RegularIcon ?? FilledIcon;
+            sender.Icon = _regular;
         }
-
-        sender.Icon = icon;
     }
 }
