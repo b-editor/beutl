a7954a127e512d8997716a9c7d71397e2a3ebc1f
indigo-san <lugongdao07@gmail.com>
2022-08-14 00:05:05 +0900

    Create FFmpegPackage.cs

---
+++ src/BeUtl.Extensions.FFmpeg/FFmpegPackage.cs
@@ -0,0 +1,15 @@
+﻿using BeUtl.Extensions.FFmpeg;
+using BeUtl.Extensions.FFmpeg.Decoding;
+using BeUtl.Framework;
+
+[assembly: PackageAware(typeof(FFmpegPackage))]
+
+namespace BeUtl.Extensions.FFmpeg;
+
+public sealed class FFmpegPackage : Package
+{
+    public override IEnumerable<Extension> GetExtensions()
+    {
+        yield return new FFmpegDecodingExtension();
+    }
+}
