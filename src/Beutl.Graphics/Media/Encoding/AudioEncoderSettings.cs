a3e277490c7123f4017aa2fcc8e96db1ed2bcc00
indigo-san <lugongdao07@gmail.com>
2022-11-21 22:18:58 +0900

    Encoding api

---
+++ src/Beutl.Graphics/Media/Encoding/AudioEncoderSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Beutl.Media.Encoding;
+
+public sealed record AudioEncoderSettings(
+    int SampleRate = 44_100,
+    int Channels = 2,
+    int Bitrate = 128_000)
+    : MediaEncoderSettings;
