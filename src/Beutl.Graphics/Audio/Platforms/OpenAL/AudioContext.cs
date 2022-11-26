dbfc3601677b18eeefc684bf36658db4ebb8d948
indigo-san <lugongdao07@gmail.com>
2022-11-26 15:30:10 +0900

    OpenAL, XAudio2のコードを 'https://github.com/b-editor/beutl/tree/old/develop/src/libraries/BEditor.Audio' から複製

---
+++ src/Beutl.Graphics/Audio/Platforms/OpenAL/AudioContext.cs
@@ -0,0 +1,188 @@
+﻿using OpenTK.Audio.OpenAL;
+
+namespace Beutl.Audio.Platforms.OpenAL;
+
+public unsafe sealed class AudioContext : IDisposable
+{
+    private readonly ALDevice _device;
+    private readonly ALContext _context;
+
+    public AudioContext()
+    {
+        _device = ALC.OpenDevice(null);
+        _context = ALC.CreateContext(_device, (int[])null!);
+
+        var alcError = ALC.GetError(_device);
+
+        if (alcError is not AlcError.NoError)
+        {
+            throw new Exception(alcError.ToString("g"));
+        }
+
+        MakeCurrent();
+    }
+
+    public bool IsDisposed { get; private set; }
+
+    public bool IsCurrent => ALC.GetCurrentContext() == _context;
+
+    public float Gain
+    {
+        get
+        {
+            ThrowIfDisposed();
+            MakeCurrent();
+            AL.GetListener(ALListenerf.Gain, out var v);
+
+            CheckError();
+
+            return v;
+        }
+        set
+        {
+            ThrowIfDisposed();
+            MakeCurrent();
+            AL.Listener(ALListenerf.Gain, value);
+
+            CheckError();
+        }
+    }
+
+    internal void CheckError()
+    {
+        var error = AL.GetError();
+
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+
+        var alcError = ALC.GetError(_device);
+
+        if (alcError is not AlcError.NoError)
+        {
+            throw new Exception(alcError.ToString("g"));
+        }
+    }
+
+    private void ThrowIfDisposed()
+    {
+        if (IsDisposed)
+        {
+            throw new ObjectDisposedException(GetType().Name);
+        }
+    }
+
+    public void MakeCurrent()
+    {
+        ThrowIfDisposed();
+
+        if (!IsCurrent)
+        {
+            ALC.MakeContextCurrent(_context);
+
+            CheckError();
+        }
+    }
+
+    public void Dispose()
+    {
+        if (IsDisposed) return;
+
+        ALC.MakeContextCurrent(ALContext.Null);
+        ALC.DestroyContext(_context);
+        ALC.CloseDevice(_device);
+        GC.SuppressFinalize(this);
+
+        IsDisposed = true;
+    }
+
+    public static int GenBuffer()
+    {
+        var buf = AL.GenBuffer();
+        ALError error = AL.GetError();
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+
+        return buf;
+    }
+
+    public static int GenSource()
+    {
+        var src = AL.GenSource();
+        ALError error = AL.GetError();
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+
+        return src;
+    }
+
+    public static void DeleteBuffer(int handle)
+    {
+        AL.DeleteBuffer(handle);
+    }
+
+    public static void DeleteSource(int handle)
+    {
+        AL.DeleteSource(handle);
+        ALError error = AL.GetError();
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+    }
+
+    public static void BufferData<TBuffer>(int bid, ALFormat format, Span<TBuffer> buffer, int freq) where TBuffer : unmanaged
+    {
+        AL.BufferData<TBuffer>(bid, format, buffer, freq);
+        ALError error = AL.GetError();
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+    }
+
+    public static void GetBuffer(int bid, ALGetBufferi param, out int value)
+    {
+        AL.GetBuffer(bid, param, out value);
+        ALError error = AL.GetError();
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+    }
+
+    public static void SourcePlay(int handle)
+    {
+        AL.SourcePlay(handle);
+        ALError error = AL.GetError();
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+    }
+
+    public static void SourceStop(int handle)
+    {
+        AL.SourceStop(handle);
+        ALError error = AL.GetError();
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+    }
+
+    public static void SourcePause(int handle)
+    {
+        AL.SourcePause(handle);
+        ALError error = AL.GetError();
+        if (error is not ALError.NoError)
+        {
+            throw new Exception(AL.GetErrorString(error));
+        }
+    }
+}
---
+++ src/Beutl.Graphics/Audio/Platforms/XAudio2/XAudioSource.cs
@@ -0,0 +1,52 @@
+﻿using Vortice.XAudio2;
+
+namespace Beutl.Audio.Platforms.XAudio2;
+
+public sealed class XAudioSource : IDisposable
+{
+    private readonly XAudioContext _context;
+    private IXAudio2SourceVoice? SourceVoice;
+
+    public XAudioSource(XAudioContext context)
+    {
+        _context = context;
+    }
+
+    public int BuffersQueued => SourceVoice?.State.BuffersQueued ?? -1;
+
+    public void Dispose()
+    {
+        SourceVoice?.DestroyVoice();
+        SourceVoice?.Dispose();
+    }
+
+    public bool IsPlaying()
+    {
+        return SourceVoice?.State.BuffersQueued > 0;
+    }
+
+    public void Play()
+    {
+        SourceVoice?.Start();
+    }
+
+    public void Stop()
+    {
+        SourceVoice?.Stop();
+    }
+
+    public void QueueBuffer(XAudioBuffer buffer)
+    {
+        if (SourceVoice == null)
+        {
+            SourceVoice = _context.Device.CreateSourceVoice(buffer.Format!);
+        }
+
+        SourceVoice.SubmitSourceBuffer(buffer.Buffer);
+    }
+
+    public void Flush()
+    {
+        SourceVoice?.FlushSourceBuffers();
+    }
+}
