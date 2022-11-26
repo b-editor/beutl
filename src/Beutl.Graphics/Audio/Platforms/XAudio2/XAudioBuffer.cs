3b7423e880105046e6b62a514c1babac4c40b83d
indigo-san <lugongdao07@gmail.com>
2022-11-26 15:30:10 +0900

    OpenAL, XAudio2のコードを 'https://github.com/b-editor/beutl/tree/old/develop/src/libraries/BEditor.Audio' から複製

---
+++ src/Beutl.Graphics/Audio/Platforms/XAudio2/XAudioBuffer.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.InteropServices;
+
+using Vortice.Multimedia;
+using Vortice.XAudio2;
+
+namespace Beutl.Audio.Platforms.XAudio2;
+
+public sealed unsafe class XAudioBuffer : IDisposable
+{
+    private void* _stream;
+
+    public XAudioBuffer()
+    {
+        Buffer = new AudioBuffer();
+    }
+
+    public AudioBuffer Buffer { get; }
+
+    public int SizeInBytes { get; private set; }
+
+    public WaveFormat? Format { get; private set; }
+
+    public unsafe void BufferData<T>(Span<T> buffer, WaveFormat format)
+        where T : unmanaged
+    {
+        int sizeInBytes = sizeof(T) * buffer.Length;
+
+        fixed (T* ptr = buffer)
+        {
+            BufferData((IntPtr)ptr, sizeInBytes, format);
+        }
+    }
+
+    public unsafe void BufferData(IntPtr buffer, int sizeInBytes, WaveFormat format)
+    {
+        if (_stream != null)
+        {
+            NativeMemory.Free(_stream);
+            _stream = null;
+        }
+
+        _stream = NativeMemory.AllocZeroed((nuint)sizeInBytes);
+        System.Buffer.MemoryCopy((void*)buffer, _stream, sizeInBytes, sizeInBytes);
+
+        Format = format;
+        SizeInBytes = sizeInBytes;
+        Buffer.AudioDataPointer = (nint)_stream;
+        Buffer.AudioBytes = SizeInBytes;
+    }
+
+    public void Dispose()
+    {
+        Buffer.Dispose();
+        if (_stream != null)
+        {
+            NativeMemory.Free(_stream);
+        }
+    }
+}
