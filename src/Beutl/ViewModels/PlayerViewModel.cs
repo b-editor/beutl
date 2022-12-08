e90a7d1a99cd1f0ccbee67a0c9345e1784590caa
indigo-san <lugongdao07@gmail.com>
2022-12-08 21:56:25 +0900

    PlayerViewModelに再生処理を追加

--- src/Beutl/ViewModels/PlayerViewModel.cs
+++ src/Beutl/ViewModels/PlayerViewModel.cs
@@ -2,17 +2,27 @@
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 
+using Beutl.Audio;
+using Beutl.Audio.Platforms.OpenAL;
+using Beutl.Audio.Platforms.XAudio2;
 using Beutl.Media;
+using Beutl.Media.Music;
+using Beutl.Media.Music.Samples;
 using Beutl.Media.Pixel;
 using Beutl.ProjectSystem;
 using Beutl.Rendering;
 
+using OpenTK.Audio.OpenAL;
+
 using Reactive.Bindings;
+
+using Vortice.Multimedia;
 
 namespace Beutl.ViewModels;
 
 public sealed class PlayerViewModel : IDisposable
 {
+    private static readonly TimeSpan s_second = TimeSpan.FromSeconds(1);
     private readonly IDisposable _disposable0;
     private readonly IDisposable _disposable1;
     private readonly ReactivePropertySlim<bool> _isEnabled;
@@ -120,6 +130,8 @@ public sealed class PlayerViewModel : IDisposable
             rate = 30;
         }
 
+        PlayAudio();
+
         TimeSpan tick = TimeSpan.FromSeconds(1d / rate);
         TimeSpan curFrame = Scene.CurrentFrame;
         TimeSpan duration = Scene.Duration;
@@ -139,6 +151,154 @@ public sealed class PlayerViewModel : IDisposable
         renderer.RenderInvalidated += Renderer_RenderInvalidated;
     }
 
+    private async void PlayAudio()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            using var audioContext = new XAudioContext();
+            await PlayWithXA2(audioContext);
+        }
+        else
+        {
+            using var audioContext = new AudioContext();
+            PlayWithOpenAL(audioContext);
+        }
+    }
+
+    private static Pcm<Stereo32BitFloat>? FillAudioData(TimeSpan f, IRenderer renderer)
+    {
+        if (renderer.RenderAudio(f).Audio is { } pcm)
+        {
+            return pcm;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private static void Swap<T>(ref T x, ref T y)
+    {
+        T temp = x;
+        x = y;
+        y = temp;
+    }
+
+    private async Task PlayWithXA2(XAudioContext audioContext)
+    {
+        IRenderer renderer = Scene.Renderer;
+        int sampleRate = renderer.Audio.SampleRate;
+        TimeSpan cur = Scene.CurrentFrame;
+        var fmt = new WaveFormat(sampleRate, 32, 2);
+        var source = new XAudioSource(audioContext);
+        var primaryBuffer = new XAudioBuffer();
+        var secondaryBuffer = new XAudioBuffer();
+
+        void PrepareBuffer(XAudioBuffer buffer)
+        {
+            Pcm<Stereo32BitFloat>? pcm = FillAudioData(cur, renderer);
+            if (pcm != null)
+            {
+                buffer.BufferData(pcm.DataSpan, fmt);
+            }
+            source.QueueBuffer(buffer);
+        }
+
+        try
+        {
+            PrepareBuffer(primaryBuffer);
+
+            cur += s_second;
+            PrepareBuffer(secondaryBuffer);
+
+            source.Play();
+
+            await Task.Delay(1000).ConfigureAwait(false);
+            // primaryBufferが終了、secondaryが開始
+
+            while (cur < Scene.Duration)
+            {
+                if (!IsPlaying.Value)
+                {
+                    source.Stop();
+                    break;
+                }
+
+                cur += s_second;
+
+                PrepareBuffer(primaryBuffer);
+
+                // バッファを入れ替える
+                Swap(ref primaryBuffer, ref secondaryBuffer);
+
+                await Task.Delay(1000).ConfigureAwait(false);
+            }
+        }
+        finally
+        {
+            source.Dispose();
+            primaryBuffer.Dispose();
+            secondaryBuffer.Dispose();
+        }
+    }
+
+    private async void PlayWithOpenAL(AudioContext audioContext)
+    {
+        audioContext.MakeCurrent();
+
+        IRenderer renderer = Scene.Renderer;
+        TimeSpan cur = Scene.CurrentFrame;
+        var buffers = AL.GenBuffers(2);
+        var source = AL.GenSource();
+
+        foreach (var buffer in buffers)
+        {
+            using var pcmf = FillAudioData(cur, renderer);
+            cur += s_second;
+            if (pcmf != null)
+            {
+                using var pcm = pcmf.Convert<Stereo16BitInteger>();
+
+                AL.BufferData<Stereo16BitInteger>(buffer, ALFormat.Stereo16, pcm.DataSpan, pcm.SampleRate);
+            }
+
+            AL.SourceQueueBuffer(source, buffer);
+        }
+
+        AL.SourcePlay(source);
+
+        while (IsPlaying.Value)
+        {
+            AL.GetSource(source, ALGetSourcei.BuffersProcessed, out var processed);
+            while (processed > 0)
+            {
+                using Pcm<Stereo32BitFloat>? pcmf = FillAudioData(cur, renderer);
+                cur += s_second;
+                int buffer = AL.SourceUnqueueBuffer(source);
+                if (pcmf != null)
+                {
+                    using var pcm = pcmf.Convert<Stereo16BitInteger>();
+
+                    AL.BufferData<Stereo16BitInteger>(buffer, ALFormat.Stereo16, pcm.DataSpan, pcm.SampleRate);
+                }
+
+                AL.SourceQueueBuffer(source, buffer);
+                processed--;
+            }
+
+            if (cur > Scene.Duration)
+                break;
+        }
+
+        while (AL.GetSourceState(source) == ALSourceState.Playing)
+        {
+            await Task.Delay(100);
+        }
+
+        AL.DeleteBuffers(buffers);
+        AL.DeleteSource(source);
+    }
+
     public void Pause()
     {
         IsPlaying.Value = false;
@@ -156,7 +316,7 @@ public sealed class PlayerViewModel : IDisposable
                 UpdateImage(bitmap);
                 bitmap.Dispose();
 
-            Scene.CurrentFrame = timeSpan;
+                Scene.CurrentFrame = timeSpan;
             }
         });
     }
