14fb215d2f7099a53b2ca6dc50695c4a99913a3e
indigo-san <lugongdao07@gmail.com>
2022-12-08 21:56:04 +0900

    XAudioContextをDisposableにした

--- src/Beutl.Graphics/Audio/Platforms/XAudio2/XAudioContext.cs
+++ src/Beutl.Graphics/Audio/Platforms/XAudio2/XAudioContext.cs
@@ -10,11 +10,12 @@ using Vortice.XAudio2;
 
 namespace Beutl.Audio.Platforms.XAudio2;
 
-public sealed partial class XAudioContext
+public sealed partial class XAudioContext : IDisposable
 {
     private const uint RPC_E_CHANGED_MODE = 0x80010106;
     private const uint COINIT_MULTITHREADED = 0x0;
     private const uint COINIT_APARTMENTTHREADED = 0x2;
+    private bool _isDisposed;
 
     [LibraryImport("ole32.dll", SetLastError = true)]
     [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
@@ -35,7 +36,23 @@ public sealed partial class XAudioContext
         MasteringVoice = Device.CreateMasteringVoice(2, 44100, AudioStreamCategory.Other);
     }
 
+    ~XAudioContext()
+    {
+        Dispose();
+    }
+
     public IXAudio2 Device { get; }
 
     public IXAudio2MasteringVoice MasteringVoice { get; }
+
+    public void Dispose()
+    {
+        if (!_isDisposed)
+        {
+            MasteringVoice.Dispose();
+            Device.Dispose();
+            _isDisposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
 }
