1456fd8d77226f6e81cd6413b388bbce584a3580
indigo-san <lugongdao07@gmail.com>
2023-04-01 18:20:04 +0900

    MediaSourceの解放をDrawable側でしないようにした

--- src/Beutl.Graphics/Graphics/SourceImage.cs
+++ src/Beutl.Graphics/Graphics/SourceImage.cs
@@ -1,11 +1,5 @@
-﻿using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Text.Json.Nodes;
-
-using Beutl.Media;
-using Beutl.Media.Pixel;
+﻿using Beutl.Media;
 using Beutl.Media.Source;
-using Beutl.Validation;
 
 namespace Beutl.Graphics;
 
@@ -13,88 +7,21 @@ public class SourceImage : Drawable
 {
     public static readonly CoreProperty<IImageSource?> SourceProperty;
     private IImageSource? _source;
-    private string? _sourceName;
 
     static SourceImage()
     {
         SourceProperty = ConfigureProperty<IImageSource?, SourceImage>(nameof(Source))
             .Accessor(o => o.Source, (o, v) => o.Source = v)
+            .DefaultValue(null)
             .Register();
 
         AffectsRender<SourceImage>(SourceProperty);
     }
 
-    [NotAutoSerialized]
     public IImageSource? Source
     {
         get => _source;
         set => SetAndRaise(SourceProperty, ref _source, value);
-    }
-
-    public override void ReadFromJson(JsonNode json)
-    {
-        base.ReadFromJson(json);
-        if (json is JsonObject jobj
-            && jobj.TryGetPropertyValue("source", out JsonNode? fileNode)
-            && fileNode is JsonValue fileValue
-            && fileValue.TryGetValue(out string? fileStr))
-        {
-            if (HierarchicalParent != null && _sourceName != fileStr)
-            {
-                Close();
-                _sourceName = fileStr;
-                Open();
-            }
-            else
-            {
-                _sourceName = fileStr;
-            }
-        }
-        else
-        {
-            _sourceName = null;
-        }
-    }
-
-    public override void WriteToJson(ref JsonNode json)
-    {
-        base.WriteToJson(ref json);
-        if (json is JsonObject jobj
-            && _source != null)
-        {
-            jobj["source"] = _source.Name;
-        }
-    }
-
-    protected override void OnAttachedToHierarchy(in HierarchyAttachmentEventArgs args)
-    {
-        base.OnAttachedToHierarchy(args);
-        Open();
-    }
-
-    protected override void OnDetachedFromHierarchy(in HierarchyAttachmentEventArgs args)
-    {
-        base.OnDetachedFromHierarchy(args);
-        Close();
-    }
-
-    private void Open()
-    {
-        if (_sourceName != null
-            && MediaSourceManager.Shared.OpenImageSource(_sourceName, out IImageSource? imageSource))
-        {
-            Source = imageSource;
-        }
-    }
-
-    private void Close()
-    {
-        if (Source != null)
-        {
-            _sourceName = Source.Name;
-            Source.Dispose();
-            Source = null;
-        }
     }
 
     protected override Size MeasureCore(Size availableSize)
