fba04605979680caf2b109f02b360636be0f0a52
indigo-san <lugongdao07@gmail.com>
2023-04-11 17:40:28 +0900

    NodeTreeBackgroundで拡大、移動したときに格子がずれるのを修正

--- src/Beutl/Views/NodeTree/NodeTreeBackground.cs
+++ src/Beutl/Views/NodeTree/NodeTreeBackground.cs
@@ -63,8 +63,8 @@ public sealed class NodeTreeBackground : Control
         int hsplit = (int)Math.Ceiling(width / DotSize) + 1;
         int vsplit = (int)Math.Ceiling(height / DotSize) + 1;
 
-        double offsetX = _zoomBorder.OffsetX;
-        double offsetY = _zoomBorder.OffsetY;
+        double offsetX = _zoomBorder.OffsetX / _zoomBorder.ZoomX;
+        double offsetY = _zoomBorder.OffsetY / _zoomBorder.ZoomY;
         double amariX = offsetX % DotSize;
         double amariY = offsetY % DotSize;
 
