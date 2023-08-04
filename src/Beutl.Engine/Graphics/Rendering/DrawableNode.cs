653ad69ca731fd6ea4d144a223e051230dedbe79
indigo-san <lugongdao07@gmail.com>
2023-08-04 12:53:51 +0900

    Beutl.GraphicsライブラリにGraphics以外の要素が含まれているので、名前をBeutl.Engineにした

---
+++ src/Beutl.Engine/Graphics/Rendering/DrawableNode.cs
@@ -0,0 +1,27 @@
+﻿using Beutl.Media;
+using Beutl.Media.Immutable;
+
+namespace Beutl.Graphics.Rendering;
+
+public class DrawableNode : ContainerNode
+{
+    public DrawableNode(Drawable drawable)
+    {
+        Drawable = drawable;
+    }
+
+    public Drawable Drawable { get; private set; }
+
+    public override void Render(ImmediateCanvas canvas)
+    {
+        base.Render(canvas);
+        Rect bounds = Bounds.Inflate(5);
+        canvas.DrawRectangle(bounds, null, new ImmutablePen(Brushes.White, null, 0, 5));
+    }
+
+    protected override void OnDispose(bool disposing)
+    {
+        base.OnDispose(disposing);
+        Drawable = null!;
+    }
+}
