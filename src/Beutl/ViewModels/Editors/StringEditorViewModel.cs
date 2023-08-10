e25e96faaaa97d3f96a17938ed877735d7a8d7b7
indigo-san <lugongdao07@gmail.com>
2023-08-10 15:05:00 +0900

    StringEditorを複数行に対応した

--- src/Beutl/ViewModels/Editors/StringEditorViewModel.cs
+++ src/Beutl/ViewModels/Editors/StringEditorViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia;
+﻿using System.ComponentModel.DataAnnotations;
+
+using Avalonia;
 
 using Beutl.Controls.PropertyEditors;
 using Beutl.Extensibility;
@@ -17,9 +19,18 @@ public sealed class StringEditorViewModel : ValueEditorViewModel<string>
         base.Accept(visitor);
         if (visitor is StringEditor editor)
         {
+            CoreProperty? prop = WrappedProperty.GetCoreProperty();
+            bool multiline = false;
+            if (prop != null)
+            {
+                CorePropertyMetadata metadata = prop.GetMetadata<CorePropertyMetadata>(WrappedProperty.ImplementedType);
+                multiline = metadata.Attributes.Any(v => v is DataTypeAttribute { DataType: DataType.MultilineText });
+            }
+
             editor[!StringEditor.TextProperty] = Value.ToBinding();
             editor.ValueChanging += OnValueChanging;
             editor.ValueChanged += OnValueChanged;
+            editor.Classes.Set("multiline", multiline);
         }
     }
 
