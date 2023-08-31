bb91a7205eec88fd358f477d6101318aacef35c0
indigo-san <lugongdao07@gmail.com>
2023-08-31 19:16:01 +0900

    Sound.Gainの単位を%にした

--- src/Beutl.Operators/Source/SourceSoundOperator.cs
+++ src/Beutl.Operators/Source/SourceSoundOperator.cs
@@ -10,6 +10,9 @@ public sealed class SourceSoundOperator : StyledSourcePublisher
     public Setter<ISoundSource?> Source { get; set; }
         = new Setter<ISoundSource?>(SourceSound.SourceProperty, null);
 
+    public Setter<float> Gain { get; set; }
+        = new Setter<float>(Sound.GainProperty, 100);
+
     protected override Style OnInitializeStyle(Func<IList<ISetter>> setters)
     {
         var style = new Style<SourceSound>();
