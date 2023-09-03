e85b2099842397b8e18f2be5b5d7d50bbe539850
indigo-san <lugongdao07@gmail.com>
2023-09-03 15:57:44 +0900

    WaveReaderを追加

---
+++ src/Beutl.Engine/Media/Wave/WaveFormat.cs
@@ -0,0 +1,16 @@
+﻿namespace Beutl.Media.Wave;
+
+public record WaveFormat
+{
+    public required WaveFormatTag FormatTag { get; init; }
+
+    public required short Channels { get; init; }
+
+    public required int SamplesPerSec { get; init; }
+
+    public required int AvgBytesPerSec { get; init; }
+
+    public required short BlockAlign { get; init; }
+
+    public required short BitsPerSample { get; init; }
+}
