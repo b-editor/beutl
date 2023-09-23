30be6b55a615c2cb4579a169340fffb61eaa46e1
indigo-san <lugongdao07@gmail.com>
2023-09-24 00:22:05 +0900

    動的なenumを追加 close ##688

---
+++ src/Beutl.Core/IDynamicEnum.cs
@@ -0,0 +1,24 @@
+﻿using Beutl.Collections;
+
+namespace Beutl;
+
+public interface IDynamicEnum : IJsonSerializable, IEquatable<IDynamicEnum?>
+{
+    IDynamicEnumValue? SelectedValue { get; }
+
+    ICoreReadOnlyList<IDynamicEnumValue> Values { get; }
+
+    IDynamicEnum WithNewValue(IDynamicEnumValue? value);
+
+    bool IEquatable<IDynamicEnum?>.Equals(IDynamicEnum? other)
+    {
+        return (SelectedValue?.Equals(other?.SelectedValue) == true
+            || (other?.SelectedValue == null && SelectedValue == null))
+            && other?.GetType() == GetType();
+    }
+}
+
+public interface IDynamicEnumValue : IEquatable<IDynamicEnumValue?>
+{
+    string DisplayName { get; }
+}
