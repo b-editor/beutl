c1051efe932a843073fd5e835702139f2e6f7e2f
indigo-san <lugongdao07@gmail.com>
2023-09-24 15:27:38 +0900

    ExtensionProvider.AllExtensionを観測可能にした

--- src/Beutl.Api/Services/ExtensionProvider.cs
+++ src/Beutl.Api/Services/ExtensionProvider.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Concurrent;
-
-using Beutl.Collections;
+﻿using Beutl.Collections;
 using Beutl.Configuration;
 using Beutl.Extensibility;
 
@@ -10,9 +8,11 @@ namespace Beutl.Api.Services;
 
 public sealed class ExtensionProvider : IBeutlApiResource
 {
-    private readonly ConcurrentDictionary<int, Extension[]> _allExtensions = new();
+    private readonly Dictionary<int, Extension[]> _allExtensions = new();
     private readonly ExtensionConfig _config = GlobalConfiguration.Instance.ExtensionConfig;
     private readonly Dictionary<Type, Array> _cache = new();
+    private readonly CoreList<Extension> _extensions = new();
+    private readonly object _lock = new();
     private bool _cacheInvalidated;
 
     public ExtensionProvider()
@@ -21,96 +21,113 @@ public sealed class ExtensionProvider : IBeutlApiResource
 
     public static ExtensionProvider Current { get; } = new();
 
-    public IEnumerable<Extension> AllExtensions => _allExtensions.Values.SelectMany(ext => ext);
+    public ICoreReadOnlyList<Extension> AllExtensions => _extensions;
 
     public TExtension[] GetExtensions<TExtension>()
         where TExtension : Extension
     {
-        if (_cacheInvalidated)
+        lock (_lock)
         {
-            _cache.Clear();
-            _cacheInvalidated = false;
-        }
+            if (_cacheInvalidated)
+            {
+                _cache.Clear();
+                _cacheInvalidated = false;
+            }
 
-        if (_cache.TryGetValue(typeof(TExtension), out Array? result))
-        {
-            return (TExtension[])result;
-        }
-        else
-        {
-            TExtension[] exts = AllExtensions.OfType<TExtension>().ToArray();
-            _cache[typeof(TExtension)] = exts;
-            return exts;
+            if (_cache.TryGetValue(typeof(TExtension), out Array? result))
+            {
+                return (TExtension[])result;
+            }
+            else
+            {
+                TExtension[] exts = AllExtensions.OfType<TExtension>().ToArray();
+                _cache[typeof(TExtension)] = exts;
+                return exts;
+            }
         }
     }
 
     public EditorExtension? MatchEditorExtension(string file)
     {
-        string? fileExt = Path.GetExtension(file);
-
-        if (_config.EditorExtensions.TryGetValue(fileExt, out ICoreList<TypeLazy>? list))
+        lock (_lock)
         {
-            foreach (Extension extension in AllExtensions)
-            {
-                Type extType = extension.GetType();
-                if (extension is not EditorExtension editorExtension) continue;
+            string? fileExt = Path.GetExtension(file);
 
-                foreach (TypeLazy type in list.GetMarshal().Value)
+            if (_config.EditorExtensions.TryGetValue(fileExt, out ICoreList<TypeLazy>? list))
+            {
+                foreach (Extension extension in AllExtensions)
                 {
-                    if (extType == type.Type
-                        && editorExtension.IsSupported(file))
+                    Type extType = extension.GetType();
+                    if (extension is not EditorExtension editorExtension) continue;
+
+                    foreach (TypeLazy type in list.GetMarshal().Value)
                     {
-                        return editorExtension;
+                        if (extType == type.Type
+                            && editorExtension.IsSupported(file))
+                        {
+                            return editorExtension;
+                        }
                     }
                 }
             }
-        }
 
-        foreach (Extension extension in AllExtensions)
-        {
-            if (extension is EditorExtension editorExtension &&
-                editorExtension.IsSupported(file))
+            foreach (Extension extension in AllExtensions)
             {
-                return editorExtension;
+                if (extension is EditorExtension editorExtension &&
+                    editorExtension.IsSupported(file))
+                {
+                    return editorExtension;
+                }
             }
-        }
 
-        return null;
+            return null;
+        }
     }
 
     public ProjectItemExtension? MatchProjectItemExtension(string file)
     {
-        foreach (Extension extension in AllExtensions)
+        lock (_lock)
         {
-            if (extension is ProjectItemExtension wsiExtension &&
-                wsiExtension.IsSupported(file))
+            foreach (Extension extension in AllExtensions)
             {
-                return wsiExtension;
+                if (extension is ProjectItemExtension wsiExtension &&
+                    wsiExtension.IsSupported(file))
+                {
+                    return wsiExtension;
+                }
             }
-        }
 
-        return null;
+            return null;
+        }
     }
 
     public IEnumerable<ProjectItemExtension> MatchProjectItemExtensions(string file)
     {
-        foreach (Extension extension in AllExtensions)
+        lock (_lock)
         {
-            if (extension is ProjectItemExtension wsiExtension &&
-                wsiExtension.IsSupported(file))
+            foreach (Extension extension in AllExtensions)
             {
-                yield return wsiExtension;
+                if (extension is ProjectItemExtension wsiExtension &&
+                    wsiExtension.IsSupported(file))
+                {
+                    yield return wsiExtension;
+                }
             }
         }
     }
 
     public void AddExtensions(int id, Extension[] extensions)
     {
-        if (!_allExtensions.TryAdd(id, extensions))
+        lock (_lock)
         {
-            throw new Exception("");
+            if (!_allExtensions.TryAdd(id, extensions))
+            {
+                throw new Exception("");
+            }
+
+            _extensions.AddRange(extensions);
+            InvalidateCache();
         }
-        InvalidateCache();
     }
 
     public void InvalidateCache()
