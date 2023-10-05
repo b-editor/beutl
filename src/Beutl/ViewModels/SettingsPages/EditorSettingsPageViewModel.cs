a3ebf8ca7124e3a753cf88be2dfdd08008798bbb
indigo-san <lugongdao07@gmail.com>
2023-10-06 00:59:49 +0900

    ドラッグ中にマウスが画面外に近づいた場合、反対側に移動するかの設定を追加

--- src/Beutl/ViewModels/SettingsPages/EditorSettingsPageViewModel.cs
+++ src/Beutl/ViewModels/SettingsPages/EditorSettingsPageViewModel.cs
@@ -17,6 +17,9 @@ public sealed class EditorSettingsPageViewModel
         AutoAdjustSceneDuration = _editorConfig.GetObservable(EditorConfig.AutoAdjustSceneDurationProperty).ToReactiveProperty();
         AutoAdjustSceneDuration.Subscribe(b => _editorConfig.AutoAdjustSceneDuration = b);
 
+        AdjustOutOfScreenCursor = _editorConfig.GetObservable(EditorConfig.AdjustOutOfScreenCursorProperty).ToReactiveProperty();
+        AdjustOutOfScreenCursor.Subscribe(b => _editorConfig.AdjustOutOfScreenCursor = b);
+
         ShowExactBoundaries = _viewConfig.GetObservable(ViewConfig.ShowExactBoundariesProperty).ToReactiveProperty();
         ShowExactBoundaries.Subscribe(b => _viewConfig.ShowExactBoundaries = b);
 
@@ -30,6 +33,8 @@ public sealed class EditorSettingsPageViewModel
     }
 
     public ReactiveProperty<bool> AutoAdjustSceneDuration { get; }
+    
+    public ReactiveProperty<bool> AdjustOutOfScreenCursor { get; }
 
     public ReactiveProperty<bool> ShowExactBoundaries { get; }
 
