f3e875dbece45d35f83b95abf95f1ebeaf8c022d
Yuto Terada (indigo-san) <lugongdao07@gmail.com>
2024-03-29 16:23:17 +0900

    MediaEncoderSettingsでCoreObjectを継承するようにした

--- src/Beutl.Engine/Media/Encoding/AudioEncoderSettings.cs
+++ src/Beutl.Engine/Media/Encoding/AudioEncoderSettings.cs
@@ -1,7 +1,42 @@
 ﻿namespace Beutl.Media.Encoding;
 
-public sealed record AudioEncoderSettings(
-    int SampleRate = 44_100,
-    int Channels = 2,
-    int Bitrate = 128_000)
-    : MediaEncoderSettings;
+public class AudioEncoderSettings : MediaEncoderSettings
+{
+    public static readonly CoreProperty<int> SampleRateProperty;
+    public static readonly CoreProperty<int> ChannelsProperty;
+    public static readonly CoreProperty<int> BitrateProperty;
+
+    static AudioEncoderSettings()
+    {
+        SampleRateProperty = ConfigureProperty<int, AudioEncoderSettings>(nameof(SampleRate))
+            .DefaultValue(44100)
+            .Register();
+
+        ChannelsProperty = ConfigureProperty<int, AudioEncoderSettings>(nameof(Channels))
+            .DefaultValue(2)
+            .Register();
+
+        BitrateProperty = ConfigureProperty<int, AudioEncoderSettings>(nameof(Bitrate))
+            .DefaultValue(128_000)
+            .Register();
+    }
+
+    public int SampleRate
+    {
+        get => GetValue(SampleRateProperty);
+        set => SetValue(SampleRateProperty, value);
+    }
+
+    public int Channels
+    {
+        get => GetValue(ChannelsProperty);
+        set => SetValue(ChannelsProperty, value);
+    }
+
+    public int Bitrate
+    {
+        get => GetValue(BitrateProperty);
+        set => SetValue(BitrateProperty, value);
+    }
+}
+
--- src/Beutl.Engine/Media/Encoding/VideoEncoderSettings.cs
+++ src/Beutl.Engine/Media/Encoding/VideoEncoderSettings.cs
@@ -1,9 +1,65 @@
-﻿namespace Beutl.Media.Encoding;
+﻿using System.ComponentModel;
 
-public sealed record VideoEncoderSettings(
-    PixelSize SourceSize,
-    PixelSize DestinationSize,
-    Rational FrameRate,
-    int Bitrate = 5_000_000,
-    int KeyframeRate = 12)
-    : MediaEncoderSettings;
+namespace Beutl.Media.Encoding;
+
+public class VideoEncoderSettings : MediaEncoderSettings
+{
+    public static readonly CoreProperty<PixelSize> SourceSizeProperty;
+    public static readonly CoreProperty<PixelSize> DestinationSizeProperty;
+    public static readonly CoreProperty<Rational> FrameRateProperty;
+    public static readonly CoreProperty<int> BitrateProperty;
+    public static readonly CoreProperty<int> KeyframeRateProperty;
+
+    static VideoEncoderSettings()
+    {
+        SourceSizeProperty = ConfigureProperty<PixelSize, VideoEncoderSettings>(nameof(SourceSize))
+            .Register();
+
+        DestinationSizeProperty = ConfigureProperty<PixelSize, VideoEncoderSettings>(nameof(DestinationSize))
+            .Register();
+
+        FrameRateProperty = ConfigureProperty<Rational, VideoEncoderSettings>(nameof(FrameRate))
+            .DefaultValue(new(30, 1))
+            .Register();
+
+        BitrateProperty = ConfigureProperty<int, VideoEncoderSettings>(nameof(Bitrate))
+            .DefaultValue(5_000_000)
+            .Register();
+
+        KeyframeRateProperty = ConfigureProperty<int, VideoEncoderSettings>(nameof(KeyframeRate))
+            .DefaultValue(12)
+            .Register();
+    }
+
+    [Browsable(false)]
+    public PixelSize SourceSize
+    {
+        get => GetValue(SourceSizeProperty);
+        set => SetValue(SourceSizeProperty, value);
+    }
+
+    public PixelSize DestinationSize
+    {
+        get => GetValue(DestinationSizeProperty);
+        set => SetValue(DestinationSizeProperty, value); 
+    }
+
+    public Rational FrameRate
+    {
+        get => GetValue(FrameRateProperty);
+        set => SetValue(FrameRateProperty, value); 
+    }
+
+    public int Bitrate
+    {
+        get => GetValue(BitrateProperty);
+        set => SetValue(BitrateProperty, value); 
+    }
+
+    public int KeyframeRate
+    {
+        get => GetValue(KeyframeRateProperty);
+        set => SetValue(KeyframeRateProperty, value); 
+    }
+}
+
