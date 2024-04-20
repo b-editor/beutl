f6802554087ba64b57f0a35d72f4135b6dfa4c3e
Yuto Terada <lugongdao07@gmail.com>
2024-04-20 20:59:43 +0900

    グラフエディタの左側に利用可能なアニメーションを表示するようにした

--- src/Beutl/ViewModels/Tools/GraphEditorTabViewModel.cs
+++ src/Beutl/ViewModels/Tools/GraphEditorTabViewModel.cs
@@ -1,21 +1,48 @@
 ﻿using System.Text.Json.Nodes;
-
 using Beutl.Animation;
+using Beutl.Operation;
 using Beutl.ProjectSystem;
 using Beutl.Services;
 using Beutl.Services.PrimitiveImpls;
-
 using Reactive.Bindings;
 
 namespace Beutl.ViewModels.Tools;
 
-public sealed class GraphEditorTabViewModel(EditViewModel editViewModel) : IToolContext
+public sealed record GraphEditorItemViewModel(string Name, KeyFrameAnimation Object);
+
+public sealed class GraphEditorTabViewModel : IToolContext
 {
+    private readonly EditViewModel _editViewModel;
+    private readonly CompositeDisposable _disposables = [];
+
+    public GraphEditorTabViewModel(EditViewModel editViewModel)
+    {
+        _editViewModel = editViewModel;
+        Element.Subscribe(_ => Refresh()).DisposeWith(_disposables);
+        SelectedAnimation = SelectedItem.CombineLatest(Element)
+            .Select(t =>
+            {
+                if (t.First == null || t.Second == null) return null;
+
+                Type type = t.First.Object.Property.PropertyType;
+                Type viewModelType = typeof(GraphEditorViewModel<>).MakeGenericType(type);
+                return (GraphEditorViewModel)Activator.CreateInstance(viewModelType, _editViewModel, t.First.Object, t.Second)!;
+            })
+            .ToReadOnlyReactivePropertySlim()
+            .DisposeWith(_disposables);
+    }
+
     public string Header => Strings.GraphEditor;
 
     public ToolTabExtension Extension => GraphEditorTabExtension.Instance;
 
-    public ReactivePropertySlim<GraphEditorViewModel?> SelectedAnimation { get; } = new();
+    public ReadOnlyReactivePropertySlim<GraphEditorViewModel?> SelectedAnimation { get; }
+
+    public ReactivePropertySlim<GraphEditorItemViewModel?> SelectedItem { get; } = new();
+
+    public ReactiveProperty<Element?> Element { get; } = new();
+
+    public CoreList<GraphEditorItemViewModel> Items { get; } = [];
 
     public IReactiveProperty<bool> IsSelected { get; } = new ReactiveProperty<bool>();
 
@@ -23,14 +50,51 @@ public sealed class GraphEditorTabViewModel(EditViewModel editViewModel) : ITool
 
     public void Dispose()
     {
-        SelectedAnimation.Dispose();
-        SelectedAnimation.Value?.Dispose();
-        SelectedAnimation.Value = null;
+        _disposables.Dispose();
     }
 
     public object? GetService(Type serviceType)
     {
         return null;
+    }
+
+    public void Refresh()
+    {
+        var selected = SelectedItem.Value;
+        if (Element.Value == null)
+        {
+            Items.Clear();
+            return;
+        }
+
+        var tmp = new List<GraphEditorItemViewModel>();
+        var searcher = new ObjectSearcher(Element.Value, v => v is KeyFrameAnimation);
+        foreach (KeyFrameAnimation anm in searcher.SearchAll().OfType<KeyFrameAnimation>())
+        {
+            var prop = anm.Property;
+            var item = new GraphEditorItemViewModel(
+                prop.GetMetadata<CorePropertyMetadata>(prop.OwnerType).DisplayAttribute?.GetName() ?? anm.Property.Name,
+                anm);
+            tmp.Add(item);
+        }
+
+        if (Items.SequenceEqual(tmp)) return;
+
+        Items.Replace(tmp);
+        SelectedItem.Value = Items.FirstOrDefault(i => i.Object == selected?.Object);
+    }
+
+    public void Select(KeyFrameAnimation? animation)
+    {
+        if (animation == null)
+        {
+            SelectedItem.Value = null;
+        }
+        else
+        {
+            Refresh();
+            SelectedItem.Value = Items.FirstOrDefault(i => i.Object == animation);
+        }
     }
 
     public void ReadFromJson(JsonObject json)
@@ -39,14 +103,13 @@ public sealed class GraphEditorTabViewModel(EditViewModel editViewModel) : ITool
         {
             if (json.TryGetPropertyValueAsJsonValue("elementId", out Guid elmId)
                 && json.TryGetPropertyValueAsJsonValue("animationId", out Guid anmId)
-                && editViewModel.Scene.FindById(elmId) is Element elm)
+                && _editViewModel.Scene.FindById(elmId) is Element elm)
             {
-                var searcher = new ObjectSearcher(elm, v => v is KeyFrameAnimation kfanm && kfanm.Id == anmId);
-                if (searcher.Search() is KeyFrameAnimation kfanm)
+                Element.Value = elm;
+                var searcher = new ObjectSearcher(elm, v => v is KeyFrameAnimation anm && anm.Id == anmId);
+                if (searcher.Search() is KeyFrameAnimation anm)
                 {
-                    Type type = kfanm.Property.PropertyType;
-                    Type viewModelType = typeof(GraphEditorViewModel<>).MakeGenericType(type);
-                    SelectedAnimation.Value = (GraphEditorViewModel)Activator.CreateInstance(viewModelType, editViewModel, kfanm, elm)!;
+                    Select(anm);
                 }
             }
         }
@@ -57,7 +120,7 @@ public sealed class GraphEditorTabViewModel(EditViewModel editViewModel) : ITool
 
     public void WriteToJson(JsonObject json)
     {
-        if (SelectedAnimation.Value is { Element.Id: { } elmId, Animation: ICoreObject { Id: { } anmId } })
+        if (SelectedAnimation.Value is { Element.Id: { } elmId, Animation: ICoreObject { Id: var anmId } })
         {
             json["elementId"] = elmId;
             json["animationId"] = anmId;
--- src/Beutl/Views/Tools/GraphEditorTab.axaml.cs
+++ src/Beutl/Views/Tools/GraphEditorTab.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Beutl.ViewModels.Tools;
 
 namespace Beutl.Views.Tools;
 public partial class GraphEditorTab : UserControl
@@ -6,5 +8,14 @@ public partial class GraphEditorTab : UserControl
     public GraphEditorTab()
     {
         InitializeComponent();
+    }
+
+    protected override void OnPointerEntered(PointerEventArgs e)
+    {
+        base.OnPointerEntered(e);
+        if (DataContext is GraphEditorTabViewModel viewModel)
+        {
+            viewModel.Refresh();
+        }
     }
 }
