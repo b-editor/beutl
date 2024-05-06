5a37199e7c68f497b2d1022b07ebc0d1f77e230a
Yuto Terada <lugongdao07@gmail.com>
2024-05-06 18:34:28 +0900

    クリッピングを修正

--- src/Beutl.Engine/Graphics/FilterEffects/Clipping.cs
+++ src/Beutl.Engine/Graphics/FilterEffects/Clipping.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.Diagnostics;
 using Beutl.Language;
-
 using SkiaSharp;
 
 namespace Beutl.Graphics.Effects;
@@ -48,7 +47,7 @@ public sealed class Clipping : FilterEffect
         {
             var target = context.Targets[i];
             var surface = target.Surface!.Value;
-            Rect originalRect = target.Bounds;
+            Rect originalRect = target.Bounds.WithX(0).WithY(0);
             Rect clipRect = originalRect.Deflate(thickness).Normalize();
 
             Rect intersect = originalRect.Intersect(clipRect);
@@ -60,13 +59,39 @@ public sealed class Clipping : FilterEffect
             }
             else
             {
-                using SKImage skimage = surface.Snapshot(SKRectI.Floor(intersect.ToSKRect()));
-                EffectTarget newtarget = context.CreateTarget(clipRect);
+                // クリッピング機能と領域展開機能をつけたらコードが汚くなりました。
+                float pointX = MathF.CopySign(MathF.Ceiling(thickness.Left) - thickness.Left, thickness.Left);
+                float pointY = MathF.CopySign(MathF.Ceiling(thickness.Top) - thickness.Top, thickness.Top);
 
-                newtarget.Surface?.Value?.Canvas?.DrawImage(skimage, intersect.X - clipRect.X, intersect.Y - clipRect.Y);
+                using SKImage skImage = surface.Snapshot(SKRectI.Floor(intersect.ToSKRect()));
+
+                var newBounds = clipRect
+                    .WithX(target.Bounds.X + thickness.Left - pointX)
+                    .WithY(target.Bounds.Y + thickness.Top - pointY);
+                if (thickness.Left > 0)
+                {
+                    // "- 1" は謎のオフセットを解消するために必要です。
+                    newBounds = newBounds.WithX(target.Bounds.X + thickness.Left + pointX - 1);
+                    pointX = 0;
+                }
+
+                if (thickness.Top > 0)
+                {
+                    newBounds = newBounds.WithY(target.Bounds.Y + thickness.Top + pointY - 1);
+                    pointY = 0;
+                }
+
+                EffectTarget newTarget = context.CreateTarget(newBounds);
+
+                newTarget.Surface?.Value?.Canvas?.DrawImage(
+                    skImage,
+                    intersect.X - clipRect.X + pointX,
+                    intersect.Y - clipRect.Y + pointY);
+
+                Debug.WriteLine(target.Bounds.X + thickness.Left - pointX);
 
                 target.Dispose();
-                context.Targets[i] = newtarget;
+                context.Targets[i] = newTarget;
             }
         }
     }
