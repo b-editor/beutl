8ad3442dc4105e2fcdd09c9e3bd4fd5582c49b91
Yuto Terada <lugongdao07@gmail.com>
2024-06-27 20:04:52 +0900

    feat: Allow version specification in build scripts

---
+++ nukebuild/VersionConfigurator.cs
@@ -0,0 +1,38 @@
+﻿public static class VersionConfigurator
+{
+    public static DotNetBuildSettings SetVersions(this DotNetBuildSettings settings, string ver, string asmVer, string infoVer)
+    {
+        return settings
+            .SetProperty("AssemblyVersion", asmVer)
+            .SetProperty("InformationalVersion", infoVer)
+            .SetProperty("IncludeSourceRevisionInInformationalVersion", false)
+            .SetProperty("Version", ver);
+    }
+
+    public static DotNetMSBuildSettings SetVersions(this DotNetMSBuildSettings settings, string ver, string asmVer, string infoVer)
+    {
+        return settings
+            .SetProperty("AssemblyVersion", asmVer)
+            .SetProperty("InformationalVersion", infoVer)
+            .SetProperty("IncludeSourceRevisionInInformationalVersion", false)
+            .SetProperty("Version", ver);
+    }
+
+    public static DotNetPackSettings SetVersions(this DotNetPackSettings settings, string ver, string asmVer, string infoVer)
+    {
+        return settings
+            .SetProperty("AssemblyVersion", asmVer)
+            .SetProperty("InformationalVersion", infoVer)
+            .SetProperty("IncludeSourceRevisionInInformationalVersion", false)
+            .SetProperty("Version", ver);
+    }
+
+    public static DotNetPublishSettings SetVersions(this DotNetPublishSettings settings, string ver, string asmVer, string infoVer)
+    {
+        return settings
+            .SetProperty("AssemblyVersion", asmVer)
+            .SetProperty("InformationalVersion", infoVer)
+            .SetProperty("IncludeSourceRevisionInInformationalVersion", false)
+            .SetProperty("Version", ver);
+    }
+}
