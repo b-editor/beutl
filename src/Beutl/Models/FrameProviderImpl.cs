433e96e1e296d4441bc9cb781c2320228675dfa6
Yuto Terada <lugongdao07@gmail.com>
2024-07-31 00:16:16 +0900

    feat: Add FrameProviderImpl and SampleProviderImpl classes

---
+++ src/Beutl/Models/FrameProviderImpl.cs
@@ -0,0 +1,51 @@
+﻿using System.Reactive.Subjects;
+using Beutl.Media;
+using Beutl.Media.Pixel;
+using Beutl.ProjectSystem;
+using Beutl.Rendering;
+
+namespace Beutl.Models;
+
+public class FrameProviderImpl(Scene scene, Rational rate, SceneRenderer renderer, Subject<TimeSpan> progress)
+    : IFrameProvider
+{
+    public long FrameCount => (long)(scene.Duration.TotalSeconds * rate.ToDouble());
+
+    public Rational FrameRate => rate;
+
+    private Bitmap<Bgra8888> RenderCore(TimeSpan time)
+    {
+        int retry = 0;
+        Retry:
+        if (renderer.Render(time))
+        {
+            return renderer.Snapshot();
+        }
+
+        if (retry > 3)
+            throw new Exception("Renderer.RenderがFalseでした。他にこのシーンを使用していないか確認してください。");
+
+        retry++;
+        goto Retry;
+    }
+
+    public async ValueTask<Bitmap<Bgra8888>> RenderFrame(long frame)
+    {
+        var time = TimeSpan.FromSeconds(frame / rate.ToDouble());
+        try
+        {
+            if (RenderThread.Dispatcher.CheckAccess())
+            {
+                return RenderCore(time);
+            }
+            else
+            {
+                return await RenderThread.Dispatcher.InvokeAsync(() => RenderCore(time));
+            }
+        }
+        finally
+        {
+            progress.OnNext(time);
+        }
+    }
+}
---
+++ src/Beutl/Models/SampleProviderImpl.cs
@@ -0,0 +1,38 @@
+﻿using System.Reactive.Subjects;
+using Beutl.Media.Music;
+using Beutl.Media.Music.Samples;
+using Beutl.ProjectSystem;
+
+namespace Beutl.Models;
+
+public class SampleProviderImpl(Scene scene, SceneComposer composer, long sampleRate, Subject<TimeSpan> progress)
+    : ISampleProvider
+{
+    public long SampleCount => (long)(scene.Duration.TotalSeconds * sampleRate);
+
+    public long SampleRate => sampleRate;
+
+    private Pcm<Stereo32BitFloat> ComposeCore(long offset)
+    {
+        return composer.Compose(TimeSpan.FromTicks(TimeSpan.TicksPerSecond * offset / sampleRate))
+               ?? throw new InvalidOperationException("composer.Composeがnullを返しました。");
+    }
+
+    public ValueTask<Pcm<Stereo32BitFloat>> Sample(long offset, long length)
+    {
+        int lengthInt = (int)length;
+        var pcm = new Pcm<Stereo32BitFloat>((int)sampleRate, lengthInt);
+        int written = 0;
+        while (written < lengthInt)
+        {
+            using var tmp = ComposeCore(offset + written);
+            var srcSpan = tmp.DataSpan;
+            var dstSpan = pcm.DataSpan;
+            srcSpan[..Math.Min(lengthInt - written, srcSpan.Length)].CopyTo(dstSpan[written..]);
+            written += srcSpan.Length;
+        }
+
+        progress.OnNext(TimeSpan.FromTicks(TimeSpan.TicksPerSecond * (offset + length) / sampleRate));
+        return ValueTask.FromResult(pcm);
+    }
+}
