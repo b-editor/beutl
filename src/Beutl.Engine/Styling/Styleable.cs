e491d7138a45164ee4d0769f792287d8eb06f80c
Yuto Terada <lugongdao07@gmail.com>
2024-09-25 01:07:28 +0900

    remove: Remove IHierarchical implementation from Styleable class

--- src/Beutl.Engine/Styling/Styleable.cs
+++ src/Beutl.Engine/Styling/Styleable.cs
@@ -10,7 +10,7 @@ using Beutl.Serialization;
 namespace Beutl.Styling;
 
 [ExcludeFromCodeCoverage]
-public abstract class Styleable : Animatable, IStyleable, IModifiableHierarchical
+public abstract class Styleable : Animatable, IStyleable
 {
     public static readonly CoreProperty<Styles> StylesProperty;
     private readonly Styles _styles;
@@ -20,10 +20,6 @@ public abstract class Styleable : Animatable, IStyleable, IModifiableHierarchica
     {
         StylesProperty = ConfigureProperty<Styles, Styleable>(nameof(Styles))
             .Accessor(o => o.Styles, (o, v) => o.Styles = v)
-            .Register();
-
-        HierarchicalParentProperty = ConfigureProperty<IHierarchical?, Styleable>(nameof(HierarchicalParent))
-            .Accessor(o => o.HierarchicalParent, (o, v) => o.HierarchicalParent = v)
             .Register();
     }
 
@@ -39,13 +35,6 @@ public abstract class Styleable : Animatable, IStyleable, IModifiableHierarchica
             item.Invalidated -= Style_Invalidated;
         };
         _styles.CollectionChanged += Style_Invalidated;
-
-        _root = this as IHierarchicalRoot;
-        _hierarchicalChildren = new CoreList<IHierarchical>()
-        {
-            ResetBehavior = ResetBehavior.Remove
-        };
-        _hierarchicalChildren.CollectionChanged += HierarchicalChildrenCollectionChanged;
     }
 
     private void Style_Invalidated(object? sender, EventArgs e)
@@ -124,175 +113,4 @@ public abstract class Styleable : Animatable, IStyleable, IModifiableHierarchica
             Styles = styles;
         }
     }
-
-    #region IHierarchical
-
-    public static readonly CoreProperty<IHierarchical?> HierarchicalParentProperty;
-    private readonly CoreList<IHierarchical> _hierarchicalChildren;
-    private IHierarchical? _parent;
-    private IHierarchicalRoot? _root;
-
-    [NotAutoSerialized]
-    public IHierarchical? HierarchicalParent
-    {
-        get => _parent;
-        private set => SetAndRaise(HierarchicalParentProperty, ref _parent, value);
-    }
-
-    protected ICoreList<IHierarchical> HierarchicalChildren => _hierarchicalChildren;
-
-    IHierarchical? IHierarchical.HierarchicalParent => _parent;
-
-    IHierarchicalRoot? IHierarchical.HierarchicalRoot => _root;
-
-    ICoreReadOnlyList<IHierarchical> IHierarchical.HierarchicalChildren => HierarchicalChildren;
-
-    public event EventHandler<HierarchyAttachmentEventArgs>? AttachedToHierarchy;
-
-    public event EventHandler<HierarchyAttachmentEventArgs>? DetachedFromHierarchy;
-
-    protected virtual void HierarchicalChildrenCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-    {
-        void SetParent(IList children)
-        {
-            int count = children.Count;
-
-            for (int i = 0; i < count; i++)
-            {
-                var logical = (IHierarchical)children[i]!;
-
-                if (logical.HierarchicalParent is null)
-                {
-                    (logical as IModifiableHierarchical)?.SetParent(this);
-                }
-            }
-        }
-
-        void ClearParent(IList children)
-        {
-            int count = children.Count;
-
-            for (int i = 0; i < count; i++)
-            {
-                var logical = (IHierarchical)children[i]!;
-
-                if (logical.HierarchicalParent == this)
-                {
-                    (logical as IModifiableHierarchical)?.SetParent(null);
-                }
-            }
-        }
-
-        switch (e.Action)
-        {
-            case NotifyCollectionChangedAction.Add:
-                SetParent(e.NewItems!);
-                break;
-
-            case NotifyCollectionChangedAction.Remove:
-                ClearParent(e.OldItems!);
-                break;
-
-            case NotifyCollectionChangedAction.Replace:
-                ClearParent(e.OldItems!);
-                SetParent(e.NewItems!);
-                break;
-
-            case NotifyCollectionChangedAction.Reset:
-                break;
-        }
-    }
-
-    protected virtual void OnAttachedToHierarchy(in HierarchyAttachmentEventArgs args)
-    {
-    }
-
-    protected virtual void OnDetachedFromHierarchy(in HierarchyAttachmentEventArgs args)
-    {
-    }
-
-    private void OnAttachedToHierarchyCore(in HierarchyAttachmentEventArgs e)
-    {
-        if (_parent == null && this is not IHierarchicalRoot)
-        {
-            throw new InvalidOperationException(
-                $"OnAttachedToHierarchyCore called for '{GetType().Name}' but element has no logical parent.");
-        }
-
-        if (_root == null)
-        {
-            _root = e.Root;
-            OnAttachedToHierarchy(e);
-            AttachedToHierarchy?.Invoke(this, e);
-        }
-
-        foreach (IHierarchical item in _hierarchicalChildren.GetMarshal().Value)
-        {
-            (item as IModifiableHierarchical)?.NotifyAttachedToHierarchy(new(e.Root, this));
-        }
-    }
-
-    private void OnDetachedFromHierarchyCore(in HierarchyAttachmentEventArgs e)
-    {
-        if (_root != null)
-        {
-            _root = null;
-            OnDetachedFromHierarchy(e);
-            DetachedFromHierarchy?.Invoke(this, e);
-
-            foreach (IHierarchical item in _hierarchicalChildren.GetMarshal().Value)
-            {
-                (item as IModifiableHierarchical)?.NotifyDetachedFromHierarchy(new(e.Root, this));
-            }
-        }
-    }
-
-    void IModifiableHierarchical.NotifyAttachedToHierarchy(in HierarchyAttachmentEventArgs e)
-    {
-        OnAttachedToHierarchyCore(e);
-    }
-
-    void IModifiableHierarchical.NotifyDetachedFromHierarchy(in HierarchyAttachmentEventArgs e)
-    {
-        OnDetachedFromHierarchyCore(e);
-    }
-
-    void IModifiableHierarchical.SetParent(IHierarchical? parent)
-    {
-        IHierarchical? old = _parent;
-
-        if (parent != old)
-        {
-            if (old != null && parent != null)
-            {
-                throw new InvalidOperationException("This logical element already has a parent.");
-            }
-
-            IHierarchicalRoot? newRoot = parent?.FindHierarchicalRoot() ?? (this as IHierarchicalRoot);
-            HierarchicalParent = parent;
-
-            if (_root != null)
-            {
-                var e = new HierarchyAttachmentEventArgs(_root, old);
-                OnDetachedFromHierarchyCore(e);
-            }
-
-            if (newRoot != null)
-            {
-                var e = new HierarchyAttachmentEventArgs(newRoot, parent);
-                OnAttachedToHierarchyCore(e);
-            }
-        }
-    }
-
-    void IModifiableHierarchical.AddChild(IHierarchical child)
-    {
-        HierarchicalChildren.Add(child);
-    }
-
-    void IModifiableHierarchical.RemoveChild(IHierarchical child)
-    {
-        HierarchicalChildren.Remove(child);
-    }
-    #endregion
 }
