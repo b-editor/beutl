2678e7131bca8a130063d09628c431aa1b8d27f0
Yuto Terada <lugongdao07@gmail.com>
2024-09-28 22:45:16 +0900

    test: Add unit tests for TypeFormat

---
+++ tests/Beutl.UnitTests/Core/TypeFormatTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Beutl.UnitTests.Core;
+
+public class TypeFormatTests
+{
+    private class MyClass;
+
+    [Test]
+    public void ToType_ReturnsCorrectType_ForValidFullName()
+    {
+        var type = TypeFormat.ToType("[Beutl.UnitTests].Core:TypeFormatTests:MyClass");
+        Assert.That(type, Is.EqualTo(typeof(MyClass)));
+    }
+
+    [Test]
+    public void ToType_ReturnsNull_ForInvalidFullName()
+    {
+        Assert.Catch(() => TypeFormat.ToType("[InvalidNamespace]InvalidType"));
+    }
+
+    [Test]
+    public void ToString_ReturnsCorrectString_ForValidType()
+    {
+        string typeString = TypeFormat.ToString(typeof(MyClass));
+        Assert.That(typeString, Is.EqualTo("[Beutl.UnitTests].Core:TypeFormatTests:MyClass"));
+    }
+}
