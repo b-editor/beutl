a296181ee8bd3c451d798dd6a24ee51a9861aeb2
Yuto Terada <lugongdao07@gmail.com>
2024-10-16 20:05:54 +0900

    feat: Add ContextCommandAttribute and ContextCommandDefinition classes

---
+++ src/Beutl.Extensibility/ContextCommandAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace Beutl.Extensibility;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ContextCommandAttribute : Attribute
+{
+    public string? Name { get; set; }
+}
+
+public class ContextCommandDefinition(
+    string name,
+    string? displayName = null,
+    string? description = null,
+    ContextCommandKeyGesture[]? keyGestures = null)
+{
+    public string Name { get; init; } = name;
+
+    public string? DisplayName { get; init; } = displayName;
+
+    public string? Description { get; init; } = description;
+
+    public ContextCommandKeyGesture[]? KeyGestures { get; init; } = keyGestures;
+}
+
+public class ContextCommandKeyGesture(string? keyGesture, PlatformID? platformId)
+{
+    public string? KeyGesture { get; init; } = keyGesture;
+
+    public PlatformID? PlatformId { get; init; } = platformId;
+}
