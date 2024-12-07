98ffbb1b7a51e4391e3d74f55ef2341f8aa4b6c2
Yuto Terada <lugongdao07@gmail.com>
2024-12-07 10:34:05 +0900

    feat: add RenderNodeCache and RenderNodeProcessor for enhanced rendering management

---
+++ src/Beutl.Engine/Graphics/Rendering/V2/RenderNodeProcessor.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.CompilerServices;
+using Beutl.Collections.Pooled;
+using Beutl.Graphics.Rendering.V2.Cache;
+
+namespace Beutl.Graphics.Rendering.V2;
+
+public class RenderNodeProcessor
+{
+    private readonly IImmediateCanvasFactory _canvasFactory;
+    private readonly ConditionalWeakTable<RenderNode, RenderNodeCache> _table = [];
+
+    public RenderNodeProcessor(RenderNode root, IImmediateCanvasFactory canvasFactory)
+    {
+        _canvasFactory = canvasFactory;
+        Root = root;
+    }
+
+    public RenderNode Root { get; set; }
+
+    public RenderNodeCache GetCache(RenderNode node)
+    {
+        return _table.GetValue(node, key => new RenderNodeCache(key));
+    }
+
+    private void IncrementRenderCount(RenderNode node)
+    {
+        var cache = GetCache(node);
+        if (node is ContainerRenderNode)
+        {
+            cache.CaptureChildren();
+        }
+
+        cache.IncrementRenderCount();
+    }
+
+    public RenderNodeOperation[] PullToRoot()
+    {
+        return Pull(Root);
+    }
+
+    public RenderNodeOperation[] Pull(RenderNode node)
+    {
+        if (node is ContainerRenderNode container)
+        {
+            using var operations = new PooledList<RenderNodeOperation>();
+            foreach (RenderNode innerNode in container.Children)
+            {
+                operations.AddRange(Pull(innerNode));
+            }
+
+            var input = operations.ToArray();
+            var context = new RenderNodeContext(_canvasFactory, input);
+            var result = node.Process(context);
+            IncrementRenderCount(node);
+            return result;
+        }
+        else
+        {
+            var context = new RenderNodeContext(_canvasFactory, []);
+            var result = node.Process(context);
+            IncrementRenderCount(node);
+            return result;
+        }
+    }
+}
