28e5e62e1b1a8c85108e52eed3b96e801982378f
Yuto Terada <lugongdao07@gmail.com>
2024-12-16 15:07:56 +0900

    feat: add properties for Width, Height, and Camera in Scene3D class with rendering adjustments

--- src/Beutl.Engine/Graphics3D/Scene3D.cs
+++ src/Beutl.Engine/Graphics3D/Scene3D.cs
@@ -1,5 +1,11 @@
-﻿using Beutl.Graphics;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using Beutl.Animation;
+using Beutl.Graphics;
 using Beutl.Graphics.Rendering;
+using Beutl.Language;
+using Beutl.Media;
+using Beutl.Serialization;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
 using SkiaSharp;
@@ -8,10 +14,76 @@ namespace Beutl.Graphics3D;
 
 public class Scene3D : Drawable
 {
+    public static readonly CoreProperty<int> WidthProperty;
+    public static readonly CoreProperty<int> HeightProperty;
+    public static readonly CoreProperty<Camera?> CameraProperty;
+    private int _width = -1;
+    private int _height = -1;
+    private Camera? _camera;
     private SceneNode? _root;
+
+    static Scene3D()
+    {
+        WidthProperty = ConfigureProperty<int, Scene3D>(nameof(Width))
+            .Accessor(o => o.Width, (o, v) => o.Width = v)
+            .DefaultValue(-1)
+            .Register();
+
+        HeightProperty = ConfigureProperty<int, Scene3D>(nameof(Height))
+            .Accessor(o => o.Height, (o, v) => o.Height = v)
+            .DefaultValue(-1)
+            .Register();
+
+        CameraProperty = ConfigureProperty<Camera?, Scene3D>(nameof(Camera))
+            .Accessor(o => o.Camera, (o, v) => o.Camera = v)
+            .Register();
+
+
+        AffectsRender<Scene3D>(
+            WidthProperty, HeightProperty, CameraProperty);
+    }
+
+    public Scene3D()
+    {
+        Camera = new PerspectiveCamera { AspectRatio = 1 };
+    }
+
+    [Display(Name = nameof(Strings.Width), ResourceType = typeof(Strings))]
+    [Range(-1, int.MaxValue)]
+    public int Width
+    {
+        get => _width;
+        set => SetAndRaise(WidthProperty, ref _width, value);
+    }
+
+    [Display(Name = nameof(Strings.Height), ResourceType = typeof(Strings))]
+    [Range(-1, int.MaxValue)]
+    public int Height
+    {
+        get => _height;
+        set => SetAndRaise(HeightProperty, ref _height, value);
+    }
+
+    public Camera? Camera
+    {
+        get => _camera;
+        set => SetAndRaise(CameraProperty, ref _camera, value);
+    }
+
+
+    public override void ApplyAnimations(IClock clock)
+    {
+        base.ApplyAnimations(clock);
+        Camera?.ApplyAnimations(clock);
+    }
 
     protected override Size MeasureCore(Size availableSize)
     {
+        if (Width > 0 && Height > 0)
+        {
+            return new Size(Width, Height);
+        }
+
         return availableSize;
     }
 
@@ -22,11 +94,32 @@ public class Scene3D : Drawable
             _root = null;
         }
 
-        _root ??= new SceneNode(new Rect(canvas.Size.ToSize(1)));
+        var size = canvas.Size;
+        if (Width > 0 && Height > 0)
+        {
+            size = new PixelSize(Width, Height);
+        }
+
+        switch (Camera)
+        {
+            case PerspectiveCamera persp:
+                persp.AspectRatio = size.Width / (float)size.Height;
+                break;
+            case OrthographicCamera ortho:
+                ortho.Width = size.Width;
+                ortho.Height = size.Height;
+                break;
+
+            default:
+                Camera = new PerspectiveCamera { AspectRatio = size.Width / (float)size.Height };
+                break;
+        }
+
+        _root ??= new SceneNode(new Rect(size.ToSize(1)), this);
         canvas.DrawNode(_root);
     }
 
-    private class SceneNode(Rect bounds) : RenderNode, IEquatable<SceneNode?>
+    private class SceneNode(Rect bounds, Scene3D scene) : RenderNode, IEquatable<SceneNode?>
     {
         private ColorBuffer? _colorBuffer;
         private DepthBuffer? _depthBuffer;
@@ -38,6 +131,7 @@ public class Scene3D : Drawable
 
         public override RenderNodeOperation[] Process(RenderNodeContext context)
         {
+            context.IsRenderCacheEnabled = false;
             return [RenderNodeOperation.CreateLambda(bounds, Render, HitTest)];
         }
 
