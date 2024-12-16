8d0d3eac7ca66773a81c59bcbdb36a3defbb267e
Yuto Terada <lugongdao07@gmail.com>
2024-12-16 15:04:07 +0900

    feat: add BoundingBox and AlignedBoxCorners structs with containment logic for 3D graphics

---
+++ src/Beutl.Engine/Graphics3D/AlignedBoxCorners.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+
+namespace Beutl.Graphics3D;
+
+public struct AlignedBoxCorners
+{
+    public Vector3 NearTopLeft;
+    public Vector3 NearTopRight;
+    public Vector3 NearBottomLeft;
+    public Vector3 NearBottomRight;
+    public Vector3 FarTopLeft;
+    public Vector3 FarTopRight;
+    public Vector3 FarBottomLeft;
+    public Vector3 FarBottomRight;
+}
---
+++ src/Beutl.Engine/Graphics3D/ContainmentType.cs
@@ -0,0 +1,8 @@
+namespace Beutl.Graphics3D;
+
+public enum ContainmentType
+{
+    Disjoint,
+    Contains,
+    Intersects,
+}
