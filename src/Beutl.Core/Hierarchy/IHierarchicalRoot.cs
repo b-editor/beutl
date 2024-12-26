3367fdaa4ac906068f71237b643d6ae777823d8c
Yuto Terada <lugongdao07@gmail.com>
2024-12-26 14:48:46 +0900

    feat: add events for descendant attachment and detachment in hierarchical structure

--- src/Beutl.Core/Hierarchy/IHierarchicalRoot.cs
+++ src/Beutl.Core/Hierarchy/IHierarchicalRoot.cs
@@ -2,4 +2,11 @@
 
 public interface IHierarchicalRoot : IHierarchical
 {
+    event EventHandler<IHierarchical> DescendantAttached;
+
+    event EventHandler<IHierarchical> DescendantDetached;
+
+    void OnDescendantAttached(IHierarchical descendant);
+
+    void OnDescendantDetached(IHierarchical descendant);
 }
