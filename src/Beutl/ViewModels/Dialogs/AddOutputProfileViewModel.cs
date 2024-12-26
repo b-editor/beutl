dadfc8194c706d8b9d3f55289d52bdf8a73843e0
Yuto Terada <lugongdao07@gmail.com>
2024-12-27 01:51:12 +0900

    change: rename AddOutputQueueDialog to AddOutputProfileDialog

---
+++ src/Beutl/ViewModels/Dialogs/AddOutputProfileViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Beutl.Services;
+using Beutl.ViewModels.Tools;
+using Reactive.Bindings;
+
+namespace Beutl.ViewModels.Dialogs;
+
+public sealed class AddOutputProfileViewModel
+{
+    private readonly OutputTabViewModel _outputTabViewModel;
+
+    public AddOutputProfileViewModel(OutputTabViewModel outputTabViewModel)
+    {
+        _outputTabViewModel = outputTabViewModel;
+        AvailableExtensions = OutputService.GetExtensions(outputTabViewModel.EditViewModel.Scene.FileName);
+
+        CanAdd = SelectedExtension.Select(x => x != null)
+            .ToReadOnlyReactivePropertySlim();
+    }
+
+    public ReadOnlyReactivePropertySlim<bool> CanAdd { get; }
+
+    public ReactiveProperty<OutputExtension?> SelectedExtension { get; } = new();
+
+    public OutputExtension[] AvailableExtensions { get; }
+
+    public void Add()
+    {
+        if (SelectedExtension.Value != null)
+        {
+            _outputTabViewModel.AddItem(SelectedExtension.Value);
+        }
+    }
+}
---
+++ src/Beutl/Views/Dialogs/AddOutputProfileDialog.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Beutl.ViewModels.Dialogs;
+using FluentAvalonia.UI.Controls;
+
+namespace Beutl.Views.Dialogs;
+
+public partial class AddOutputProfileDialog : ContentDialog
+{
+    public AddOutputProfileDialog()
+    {
+        InitializeComponent();
+    }
+
+    protected override Type StyleKeyOverride => typeof(ContentDialog);
+
+    protected override void OnPrimaryButtonClick(ContentDialogButtonClickEventArgs args)
+    {
+        base.OnPrimaryButtonClick(args);
+        if (DataContext is not AddOutputProfileViewModel vm) return;
+        vm.Add();
+    }
+}
