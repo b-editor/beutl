bf5b2ad7b5bb03bf6cf88515115359c0331538a6
Yuto Terada <lugongdao07@gmail.com>
2025-02-09 19:44:43 +0900

    feat: Add AppUpdateResponse model and update IAppClient interface for app updates

--- src/Beutl.Api/Clients/IAppClient.cs
+++ src/Beutl.Api/Clients/IAppClient.cs
@@ -6,4 +6,8 @@ public interface IAppClient
 {
     [Get("/api/v1/app/checkForUpdates/{version}")]
     Task<CheckForUpdatesResponse> CheckForUpdates(string version);
+
+
+    [Get("/api/v3/app/updates/{version}")]
+    Task<AppUpdateResponse> GetUpdate(string version, string type, string os, string arch, string standalone, string prerelease);
 }
