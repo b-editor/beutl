ae3b1e99476cee01be7bca64e0c9e3306f8aa7ed
Yuto Terada <lugongdao07@gmail.com>
2025-03-01 21:09:59 +0900

    feat: add JSON and Type converters for RelativeRect to enhance serialization support

---
+++ src/Beutl.Engine/Converters/RelativeRectConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using Beutl.Graphics;
+
+namespace Beutl.Converters;
+
+public sealed class RelativeRectConverter : TypeConverter
+{
+    public override bool CanConvertFrom(ITypeDescriptorContext? context, [NotNullWhen(true)] Type? sourceType)
+    {
+        return sourceType == typeof(string);
+    }
+
+    public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+    {
+        if (value is string str)
+        {
+            return RelativeRect.Parse(str);
+        }
+
+        return base.ConvertFrom(context, culture, value);
+    }
+}
