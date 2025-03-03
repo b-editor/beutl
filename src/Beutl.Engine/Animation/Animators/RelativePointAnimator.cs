bd592d503499237153502aa868ecf5e7d4485564
Yuto Terada <lugongdao07@gmail.com>
2025-03-03 14:28:41 +0900

    feat: add RelativePoint and RelativeRect animators for enhanced animation support

---
+++ src/Beutl.Engine/Animation/Animators/RelativePointAnimator.cs
@@ -0,0 +1,18 @@
+﻿using Beutl.Graphics;
+
+namespace Beutl.Animation.Animators;
+
+public sealed class RelativePointAnimator : Animator<RelativePoint>
+{
+    private static readonly PointAnimator s_pointAnimator = new();
+
+    public override RelativePoint Interpolate(float progress, RelativePoint oldValue, RelativePoint newValue)
+    {
+        if (oldValue.Unit != newValue.Unit)
+        {
+            return progress >= 0.5 ? newValue : oldValue;
+        }
+
+        return new RelativePoint(s_pointAnimator.Interpolate(progress, oldValue.Point, newValue.Point), oldValue.Unit);
+    }
+}
---
+++ src/Beutl.Engine/Animation/Animators/RelativeRectAnimator.cs
@@ -0,0 +1,18 @@
+﻿using Beutl.Graphics;
+
+namespace Beutl.Animation.Animators;
+
+public sealed class RelativeRectAnimator : Animator<RelativeRect>
+{
+    private static readonly RectAnimator s_rectAnimator = new();
+
+    public override RelativeRect Interpolate(float progress, RelativeRect oldValue, RelativeRect newValue)
+    {
+        if (oldValue.Unit != newValue.Unit)
+        {
+            return progress >= 0.5 ? newValue : oldValue;
+        }
+
+        return new RelativeRect(s_rectAnimator.Interpolate(progress, oldValue.Rect, newValue.Rect), oldValue.Unit);
+    }
+}
