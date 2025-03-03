a936a2b8e6e815a67e9f1e9b5251be64e5e9257f
Yuto Terada <lugongdao07@gmail.com>
2025-03-04 01:53:51 +0900

    feat: add SelectDrawableTypeViewModel for selecting drawable types in dialogs

---
+++ src/Beutl/ViewModels/Dialogs/SelectDrawableTypeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Beutl.Graphics;
+using Beutl.Services;
+
+namespace Beutl.ViewModels.Dialogs;
+
+public sealed class SelectDrawableTypeViewModel : SelectLibraryItemDialogViewModel
+{
+    public SelectDrawableTypeViewModel()
+        : base(KnownLibraryItemFormats.Drawable, typeof(Drawable))
+    {
+    }
+}
