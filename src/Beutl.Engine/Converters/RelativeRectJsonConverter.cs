6f6ac7e387828b317fa11f4ab307c19dc3b05cf4
Yuto Terada <lugongdao07@gmail.com>
2025-03-05 17:45:52 +0900

    fix: enhance RelativeRectJsonConverter to support backward compatibility with JSON object format

--- src/Beutl.Engine/Converters/RelativeRectJsonConverter.cs
+++ src/Beutl.Engine/Converters/RelativeRectJsonConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using Beutl.Graphics;
 
@@ -8,6 +9,25 @@ internal sealed class RelativeRectJsonConverter : JsonConverter<RelativeRect>
 {
     public override RelativeRect Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
+        // 互換性のため
+        if (reader.TokenType == JsonTokenType.StartObject)
+        {
+            var jsonNode = JsonNode.Parse(ref reader);
+            if (jsonNode is JsonObject jsonObject)
+            {
+                int? unit = (int?)jsonObject["Unit"];
+                string? rect = (string?)jsonObject["Rect"];
+                if (unit.HasValue && rect != null)
+                {
+                    return new RelativeRect(Rect.Parse(rect), (RelativeUnit)unit.Value);
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid RelativeRect.");
+            }
+        }
+
         string? s = reader.GetString();
         if (s == null)
             throw new Exception("Invalid RelativeRect.");
