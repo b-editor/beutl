d07529f470fad47ec4a7d9c521a970992c285037
Yuto Terada <lugongdao07@gmail.com>
2025-05-03 02:50:38 +0900

    feat: refactor StandardMaterial properties for improved material management

--- src/Beutl.Engine/Graphics3D/Materials/StandardMaterial.cs
+++ src/Beutl.Engine/Graphics3D/Materials/StandardMaterial.cs
@@ -4,63 +4,94 @@ namespace Beutl.Graphics3D;
 
 public class StandardMaterial : Material
 {
-    public static readonly CoreProperty<Color> ColorProperty;
-    public static readonly CoreProperty<float> SpecularPowerProperty;
-    public static readonly CoreProperty<float> ReflectivityProperty;
-    public static readonly CoreProperty<IBrush?> BrushProperty;
-    private Color _color = Colors.White;
-    private float _specularPower = 40.0f;
-    private float _reflectivity;
-    private IBrush? _brush;
+    public static readonly CoreProperty<Color> AlbedoProperty;
+    public static readonly CoreProperty<Color> SpecularProperty;
+    public static readonly CoreProperty<float> MetallicProperty;
+    public static readonly CoreProperty<float> SmoothnessProperty;
+    public static readonly CoreProperty<ColorF> EmissionProperty;
+    public static readonly CoreProperty<TextureSource?> HeightMapProperty;
+    public static readonly CoreProperty<TextureSource?> NormalMapProperty;
+    private Color _albedo;
+    private Color _specular;
+    private float _metallic;
+    private float _smoothness;
+    private ColorF _emission;
+    private TextureSource _heightMap;
 
     static StandardMaterial()
     {
-        ColorProperty = ConfigureProperty<Color, StandardMaterial>(nameof(Color))
-            .Accessor(o => o.Color, (o, v) => o.Color = v)
+        AlbedoProperty = ConfigureProperty<Color, StandardMaterial>(nameof(Albedo))
+            .Accessor(o => o.Albedo, (o, v) => o.Albedo = v)
             .DefaultValue(Colors.White)
             .Register();
 
-        SpecularPowerProperty = ConfigureProperty<float, StandardMaterial>(nameof(SpecularPower))
-            .Accessor(o => o.SpecularPower, (o, v) => o.SpecularPower = v)
-            .DefaultValue(40.0f)
+        SpecularProperty = ConfigureProperty<Color, StandardMaterial>(nameof(Specular))
+            .Accessor(o => o.Specular, (o, v) => o.Specular = v)
+            .DefaultValue(Colors.White)
             .Register();
 
-        ReflectivityProperty = ConfigureProperty<float, StandardMaterial>(nameof(Reflectivity))
-            .Accessor(o => o.Reflectivity, (o, v) => o.Reflectivity = v)
+        MetallicProperty = ConfigureProperty<float, StandardMaterial>(nameof(Metallic))
+            .Accessor(o => o.Metallic, (o, v) => o.Metallic = v)
+            .DefaultValue(0.0F)
             .Register();
 
-        BrushProperty = ConfigureProperty<IBrush?, StandardMaterial>(nameof(Brush))
-            .Accessor(o => o.Brush, (o, v) => o.Brush = v)
+        SmoothnessProperty = ConfigureProperty<float, StandardMaterial>(nameof(Smoothness))
+            .Accessor(o => o.Smoothness, (o, v) => o.Smoothness = v)
+            .DefaultValue(0.0F)
+            .Register();
+
+        EmissionProperty = ConfigureProperty<ColorF, StandardMaterial>(nameof(Emission))
+            .Accessor(o => o.Emission, (o, v) => o.Emission = v)
+            .DefaultValue(new ColorF
+            {
+                A = 1,
+                R = 1,
+                G = 1,
+                B = 1
+            })
+            .Register();
+
+        HeightMapProperty = ConfigureProperty<TextureSource?, StandardMaterial>(nameof(HeightMap))
+            .Accessor(o => o.HeightMap, (o, v) => o.HeightMap = v)
+            .DefaultValue(null)
             .Register();
 
         // AffectsRender<SpecularMaterial>(ColorProperty);
     }
 
-    public Color Color
+    public Color Albedo
     {
-        get => _color;
-        set => SetAndRaise(ColorProperty, ref _color, value);
+        get => _albedo;
+        set => SetAndRaise(AlbedoProperty, ref _albedo, value);
     }
 
-    public float SpecularPower
+    public Color Specular
     {
-        get => _specularPower;
-        set => SetAndRaise(SpecularPowerProperty, ref _specularPower, value);
+        get => _specular;
+        set => SetAndRaise(SpecularProperty, ref _specular, value);
     }
 
-    public float Reflectivity
+    public float Metallic
     {
-        get => _reflectivity;
-        set => SetAndRaise(ReflectivityProperty, ref _reflectivity, value);
+        get => _metallic;
+        set => SetAndRaise(MetallicProperty, ref _metallic, value);
     }
 
-    public IBrush? Brush
+    public float Smoothness
     {
-        get => _brush;
-        set => SetAndRaise(BrushProperty, ref _brush, value);
+        get => _smoothness;
+        set => SetAndRaise(SmoothnessProperty, ref _smoothness, value);
     }
 
-    public StandardMaterial()
+    public ColorF Emission
     {
+        get => _emission;
+        set => SetAndRaise(EmissionProperty, ref _emission, value);
+    }
+
+    public TextureSource HeightMap
+    {
+        get => _heightMap;
+        set => SetAndRaise(HeightMapProperty, ref _heightMap, value);
     }
 }
