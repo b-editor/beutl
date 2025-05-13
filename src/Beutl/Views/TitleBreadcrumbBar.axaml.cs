f6cc15f7f28a25200a8ec9092e484dbecb3bd13d
Yuto Terada <lugongdao07@gmail.com>
2025-05-14 00:29:49 +0900

    feat: enhance TitleBreadcrumbBar with button click handling and flyout management

--- src/Beutl/Views/TitleBreadcrumbBar.axaml.cs
+++ src/Beutl/Views/TitleBreadcrumbBar.axaml.cs
@@ -1,4 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Beutl.ViewModels;
 
 namespace Beutl.Views;
 
@@ -8,5 +13,36 @@ public partial class TitleBreadcrumbBar : UserControl
     {
         InitializeComponent();
     }
-}
 
+    private void OnButtonClick(object? sender, RoutedEventArgs e)
+    {
+        // Commandプロパティを使わない理由
+        // - flyout.Hideを実行するとbuttonのDataContextとCommandがnullになり実行されなくなってしまうため
+        if (sender is Button button && DataContext is TitleBreadcrumbBarViewModel viewModel)
+        {
+            switch (button.Tag)
+            {
+                case "OpenFile":
+                    viewModel.OpenFile.Execute(null);
+                    break;
+                case "NewScene":
+                    viewModel.NewScene.Execute(null);
+                    break;
+            }
+        }
+
+        if (FileButton.Flyout is Flyout flyout)
+        {
+            flyout.Hide();
+        }
+    }
+
+    private void OnListBoxTapped(object? sender, TappedEventArgs e)
+    {
+        if (FileButton.Flyout is not Flyout flyout) return;
+        if ((e.Source as StyledElement)?.GetSelfAndLogicalAncestors().Any(i => i is ListBoxItem) == true)
+        {
+            flyout.Hide();
+        }
+    }
+}
