eb95611b6813994a6444ca166124b28fc4d6bf8a
Yuto Terada <lugongdao07@gmail.com>
2025-05-18 11:22:21 +0900

    feat: add StartInput property and UI binding for scene start time

--- src/Beutl/ViewModels/Tools/SceneSettingsTabViewModel.cs
+++ src/Beutl/ViewModels/Tools/SceneSettingsTabViewModel.cs
@@ -24,6 +24,10 @@ public sealed class SceneSettingsTabViewModel : IToolContext
         Height = frameSize.Select(v => v.Height)
             .ToReactiveProperty()
             .DisposeWith(_disposable);
+        StartInput = _scene.GetObservable(Scene.StartProperty)
+            .Select(v => v.ToString())
+            .ToReactiveProperty()
+            .DisposeWith(_disposable)!;
         DurationInput = _scene.GetObservable(Scene.DurationProperty)
             .Select(v => v.ToString())
             .ToReactiveProperty()
@@ -35,35 +39,22 @@ public sealed class SceneSettingsTabViewModel : IToolContext
         Width.SetValidateNotifyError(ValidateSize);
         Height.SetValidateNotifyError(ValidateSize);
         LayerCount.SetValidateNotifyError(ValidateSize);
-        DurationInput.SetValidateNotifyError(str =>
-        {
-            if (TimeSpan.TryParse(str, out TimeSpan time))
-            {
-                if (time <= TimeSpan.Zero)
-                {
-                    return Message.ValueLessThanOrEqualToZero;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            else
-            {
-                return Message.InvalidString;
-            }
-        });
 
-        CanApply = Width.CombineLatest(Height, DurationInput, LayerCount).Select(t =>
+        StartInput.SetValidateNotifyError(TimeSpanValidator);
+        DurationInput.SetValidateNotifyError(TimeSpanValidator);
+
+        CanApply = Width.CombineLatest(Height, StartInput, DurationInput, LayerCount).Select(t =>
             {
                 int width = t.First;
                 int height = t.Second;
 
-                string time = t.Third;
+                string startTime = t.Third;
+                string durationTime = t.Fourth;
                 return width > 0 &&
                     height > 0 &&
-                    TimeSpan.TryParse(time, out TimeSpan ts) && ts > TimeSpan.Zero &&
-                    t.Fourth > 0;
+                    TimeSpan.TryParse(startTime, out TimeSpan ts1) && ts1 > TimeSpan.Zero &&
+                    TimeSpan.TryParse(durationTime, out TimeSpan ts2) && ts2 > TimeSpan.Zero &&
+                    t.Fifth > 0;
             })
             .ToReadOnlyReactivePropertySlim()
             .DisposeWith(_disposable);
@@ -71,15 +62,18 @@ public sealed class SceneSettingsTabViewModel : IToolContext
         Apply = new ReactiveCommand(CanApply)
             .WithSubscribe(() =>
             {
-                if (TimeSpan.TryParse(DurationInput.Value, out TimeSpan ts))
+                if (TimeSpan.TryParse(StartInput.Value, out TimeSpan start)
+                    &&TimeSpan.TryParse(DurationInput.Value, out TimeSpan duration))
                 {
                     if (Width.Value != _scene.FrameSize.Width
                         || Height.Value != _scene.FrameSize.Height
-                        || ts != _scene.Duration)
+                        || start != _scene.Start
+                        || duration != _scene.Duration)
                     {
                         CommandRecorder recorder = _editViewModel.CommandRecorder;
                         RecordableCommands.Edit(_scene, Scene.FrameSizeProperty, new(Width.Value, Height.Value))
-                            .Append(RecordableCommands.Edit(_scene, Scene.DurationProperty, ts))
+                            .Append(RecordableCommands.Edit(_scene, Scene.StartProperty, start))
+                            .Append(RecordableCommands.Edit(_scene, Scene.DurationProperty, duration))
                             .WithStoables([_scene])
                             .DoAndRecord(recorder);
                     }
@@ -97,10 +91,31 @@ public sealed class SceneSettingsTabViewModel : IToolContext
             {
                 Width.Value = _scene.FrameSize.Width;
                 Height.Value = _scene.FrameSize.Height;
+                StartInput.Value = _scene.Start.ToString();
                 DurationInput.Value = _scene.Duration.ToString();
                 LayerCount.Value = _editViewModel.Options.Value.MaxLayerCount;
             })
             .DisposeWith(_disposable);
+        return;
+    }
+
+    private static string? TimeSpanValidator(string str)
+    {
+        if (TimeSpan.TryParse(str, out TimeSpan time))
+        {
+            if (time <= TimeSpan.Zero)
+            {
+                return Message.ValueLessThanOrEqualToZero;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return Message.InvalidString;
+        }
     }
 
     public ReactiveCommand Apply { get; }
@@ -110,6 +125,8 @@ public sealed class SceneSettingsTabViewModel : IToolContext
     public ReactiveProperty<int> Width { get; }
 
     public ReactiveProperty<int> Height { get; }
+
+    public ReactiveProperty<string> StartInput { get; }
 
     public ReactiveProperty<string> DurationInput { get; }
 
