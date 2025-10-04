08381b3e5d37c157b75b466f7b4b8c1c2e0da65c
Yuto Terada <lugongdao07@gmail.com>
2025-10-05 00:13:54 +0900

    feat: add RenderContext class for clock-based property retrieval

---
+++ src/Beutl.Engine/Graphics/Rendering/RenderContext.cs
@@ -0,0 +1,16 @@
+﻿using Beutl.Animation;
+using Beutl.Engine;
+
+namespace Beutl.Graphics.Rendering;
+
+public class RenderContext(IClock clock)
+{
+    public IClock Clock => clock;
+
+    public T Get<T>(IProperty<T> property)
+    {
+        if (property == null)
+            throw new ArgumentNullException(nameof(property));
+        return property.GetValue(Clock);
+    }
+}
