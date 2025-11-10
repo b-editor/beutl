d46dd921d5d29f249b235cc079d4a133675ddeaf
Yuto Terada <lugongdao07@gmail.com>
2025-11-11 01:11:26 +0900

    feat: Implement IFileSystem interface and local/virtual file system classes

---
+++ src/Beutl.Core/IO/IFileSystem.cs
@@ -0,0 +1,26 @@
+﻿using Beutl.Serialization;
+
+namespace Beutl.IO;
+
+public interface IFileSystem
+{
+    Uri[] GetFiles(Uri uri, string searchPattern, bool recursive);
+
+    Uri[] GetDirectory(Uri uri, string searchPattern, bool recursive);
+
+    bool FileExists(Uri uri);
+
+    bool DirectoryExists(Uri uri);
+
+    void CreateDirectory(Uri uri);
+
+    void DeleteFile(Uri uri);
+
+    void DeleteDirectory(Uri uri, bool recursive);
+
+    Stream CreateFile(Uri uri);
+
+    Stream OpenFile(Uri uri);
+}
+
+// 仮想ファイルシステム Dictionaryに保存する
