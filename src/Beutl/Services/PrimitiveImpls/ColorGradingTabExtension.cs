9c6cd0cef159e4b6b93d19b64c3515e43c999652
Yuto Terada <lugongdao07@gmail.com>
2025-12-16 21:41:25 +0900

    feat: add ColorGradingTab and related components for enhanced color grading functionality

---
+++ src/Beutl/Services/PrimitiveImpls/ColorGradingTabExtension.cs
@@ -0,0 +1,58 @@
+using System.Diagnostics.CodeAnalysis;
+
+using Avalonia.Controls;
+using Beutl.ViewModels;
+using Beutl.ViewModels.Tools;
+using Beutl.Views.Tools;
+using FluentAvalonia.UI.Controls;
+using SymbolIconSource = FluentIcons.FluentAvalonia.SymbolIconSource;
+using Symbol = FluentIcons.Common.Symbol;
+
+namespace Beutl.Services.PrimitiveImpls;
+
+[PrimitiveImpl]
+public sealed class ColorGradingTabExtension : ToolTabExtension
+{
+    public static readonly ColorGradingTabExtension Instance = new();
+
+    public override bool CanMultiple => true;
+
+    public override string Name => "Color Grading Tab";
+
+    public override string DisplayName => Strings.ColorGrading;
+
+    public override string Header => Strings.ColorGrading;
+
+    public override IconSource GetIcon()
+    {
+        return new SymbolIconSource { Symbol = Symbol.ColorBackground };
+    }
+
+    public override bool TryCreateContent(IEditorContext editorContext, [NotNullWhen(true)] out Control? control)
+    {
+        if (editorContext is EditViewModel)
+        {
+            control = new ColorGradingTab();
+            return true;
+        }
+        else
+        {
+            control = null;
+            return false;
+        }
+    }
+
+    public override bool TryCreateContext(IEditorContext editorContext, [NotNullWhen(true)] out IToolContext? context)
+    {
+        if (editorContext is EditViewModel editViewModel)
+        {
+            context = new ColorGradingTabViewModel(editViewModel);
+            return true;
+        }
+        else
+        {
+            context = null;
+            return false;
+        }
+    }
+}
---
+++ src/Beutl/Views/Tools/ColorGradingTab.axaml.cs
@@ -0,0 +1,71 @@
+using Avalonia;
+using Avalonia.Controls;
+
+using Beutl.Controls.PropertyEditors;
+using Beutl.ViewModels.Editors;
+using Beutl.ViewModels.Tools;
+using Beutl.Views.Editors;
+
+namespace Beutl.Views.Tools;
+
+public partial class ColorGradingTab : UserControl
+{
+    private readonly CompositeDisposable _subscriptions = [];
+
+    public ColorGradingTab()
+    {
+        InitializeComponent();
+
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        _subscriptions.Clear();
+        if (DataContext is ColorGradingTabViewModel vm)
+        {
+            _subscriptions.Add(vm.TemperatureEditor.Subscribe(v => BindNumberEditor(TemperatureEditor, v)));
+            _subscriptions.Add(vm.TintEditor.Subscribe(v => BindNumberEditor(TintEditor, v)));
+            _subscriptions.Add(vm.ExposureEditor.Subscribe(v => BindNumberEditor(ExposureEditor, v)));
+            _subscriptions.Add(vm.ContrastEditor.Subscribe(v => BindNumberEditor(ContrastEditor, v)));
+            _subscriptions.Add(vm.ContrastPivotEditor.Subscribe(v => BindNumberEditor(ContrastPivotEditor, v)));
+            _subscriptions.Add(vm.SaturationEditor.Subscribe(v => BindNumberEditor(SaturationEditor, v)));
+            _subscriptions.Add(vm.HueEditor.Subscribe(v => BindNumberEditor(HueEditor, v)));
+
+            _subscriptions.Add(vm.ShadowsEditor.Subscribe(v => BindColorPicker(ShadowsPicker, v)));
+            _subscriptions.Add(vm.MidtonesEditor.Subscribe(v => BindColorPicker(MidtonesPicker, v)));
+            _subscriptions.Add(vm.HighlightsEditor.Subscribe(v => BindColorPicker(HighlightsPicker, v)));
+            _subscriptions.Add(vm.LiftEditor.Subscribe(v => BindColorPicker(LiftPicker, v)));
+            _subscriptions.Add(vm.GammaEditor.Subscribe(v => BindColorPicker(GammaPicker, v)));
+            _subscriptions.Add(vm.GainEditor.Subscribe(v => BindColorPicker(GainPicker, v)));
+            _subscriptions.Add(vm.OffsetEditor.Subscribe(v => BindColorPicker(OffsetPicker, v)));
+        }
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+        _subscriptions.Dispose();
+    }
+
+    private void BindNumberEditor(NumberEditor<float> editor, NumberEditorViewModel<float>? viewModel)
+    {
+        editor.MenuContent = new PropertyEditorMenu();
+        if (viewModel != null)
+        {
+            viewModel.Accept(editor);
+        }
+    }
+
+    private void BindColorPicker(ColorGradingWheel picker, GradingColorEditorViewModel? viewModel)
+    {
+        if (viewModel == null)
+        {
+            picker.IsEnabled = false;
+            return;
+        }
+
+        picker.MenuContent = new PropertyEditorMenu();
+        viewModel.Accept(picker);
+    }
+}
