50b65c174b05e37c7433c56f820e05f0b98c50e0
Yuto Terada <lugongdao07@gmail.com>
2026-01-19 04:56:07 +0900

    feat(graphics): add Model3D and ModelSource classes for 3D model handling

---
+++ src/Beutl.Engine/Converters/Vertex3DJsonConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Beutl.Graphics3D.Meshes;
+
+namespace Beutl.Converters;
+
+internal sealed class Vertex3DJsonConverter : JsonConverter<Vertex3D>
+{
+    public override Vertex3D Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? s = reader.GetString();
+        if (s == null)
+            throw new Exception("Invalid Vertex3D.");
+
+        return Vertex3D.Parse(s);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Vertex3D value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
