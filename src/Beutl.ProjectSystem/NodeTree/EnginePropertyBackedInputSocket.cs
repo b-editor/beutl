ae73ecdfc3994f2941788fb0fba9df8a2c790043
Yuto Terada <lugongdao07@gmail.com>
2026-02-08 16:19:42 +0900

    feat: implement EnginePropertyBackedInputSocket with property adapters and JSON serialization support

---
+++ src/Beutl.ProjectSystem/NodeTree/EnginePropertyBackedInputSocket.cs
@@ -0,0 +1,87 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Beutl.Engine;
+using Beutl.Engine.Expressions;
+using Beutl.Extensibility;
+using Beutl.Operation;
+
+namespace Beutl.NodeTree;
+
+public class EnginePropertyBackedInputSocket<T> : InputSocket<T>
+{
+    private readonly IProperty<T> _property;
+
+    public EnginePropertyBackedInputSocket(EngineObject obj, IProperty<T> property)
+    {
+        Name = property.Name;
+        Display = property.GetPropertyInfo()?.GetCustomAttribute<DisplayAttribute>();
+        _property = property;
+        IPropertyAdapter<T> adapter;
+        if (property is AnimatableProperty<T> animatableProperty)
+        {
+            adapter = new AnimatablePropertyAdapter<T>(animatableProperty, obj);
+        }
+        else if (property is SimpleProperty<T> simpleProperty)
+        {
+            adapter = new SimplePropertyAdapter<T>(simpleProperty, obj);
+        }
+        else
+        {
+            adapter = new EnginePropertyAdapter<T>(property, obj);
+        }
+
+        Property = adapter;
+        Connected += (_, _) => _property.Expression = new SocketExpression();
+        Disconnected += (_, _) => _property.Expression = null;
+    }
+
+    // デフォルトではPropertyAdapterのアニメーションが実行されてしまうので防ぐ
+    public override void PreEvaluate(EvaluationContext context)
+    {
+    }
+
+    public override void Evaluate(EvaluationContext context)
+    {
+        if (Connection != null && _property.Expression is SocketExpression exp)
+        {
+            exp.Value = Value;
+        }
+    }
+
+    [JsonConverter(typeof(SocketExpressionJsonConverter))]
+    public class SocketExpression : IExpression<T>
+    {
+        public T? Value { get; set; }
+
+        public string ExpressionString => "[Socket Connected]";
+
+        public bool Validate(out string? error)
+        {
+            error = null;
+            return true;
+        }
+
+        public T Evaluate(ExpressionContext context)
+        {
+            return Value!;
+        }
+    }
+}
+
+// 空のオブジェクトを書き込み、空のExpressionを生成するだけのコンバーター
+internal class SocketExpressionJsonConverter : JsonConverter<IExpression>
+{
+    public override IExpression? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        // typeToConvertはSocketExpression
+        return Activator.CreateInstance(typeToConvert) as IExpression;
+    }
+
+    public override void Write(Utf8JsonWriter writer, IExpression value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        writer.WriteEndObject();
+    }
+}
