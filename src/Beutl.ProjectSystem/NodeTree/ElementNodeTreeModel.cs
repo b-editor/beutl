2f001cc446bb144580ab0532302580114e5b2823
Yuto Terada <lugongdao07@gmail.com>
2026-02-15 04:07:54 +0900

    refactor: update NodeTreeModel and ElementNodeTreeModel for new rendering system

--- src/Beutl.ProjectSystem/NodeTree/ElementNodeTreeModel.cs
+++ src/Beutl.ProjectSystem/NodeTree/ElementNodeTreeModel.cs
@@ -1,45 +1,37 @@
-﻿using Beutl.Animation;
+﻿using System.ComponentModel;
 using Beutl.Collections.Pooled;
 using Beutl.Engine;
 using Beutl.Graphics.Rendering;
 using Beutl.Media;
+using Beutl.NodeTree.Rendering;
 using Beutl.ProjectSystem;
 
 namespace Beutl.NodeTree;
 
 public class ElementNodeTreeModel : NodeTreeModel
 {
-    private readonly NodeTreeEvaluator _evaluator;
+    private readonly NodeTreeSnapshot _snapshot = new();
+    private Element? _element;
 
     public ElementNodeTreeModel()
     {
-        _evaluator = new NodeTreeEvaluator(this);
+        TopologyChanged += (_, _) => _snapshot.MarkDirty();
     }
 
     public PooledList<EngineObject> Evaluate(EvaluationTarget target, IRenderer renderer, Element element)
     {
-        _ = target;
-        _evaluator.Build(renderer);
+        _snapshot.Build(this, renderer);
 
         var list = new PooledList<EngineObject>();
         try
         {
-            foreach (NodeEvaluationContext[]? item in _evaluator.EvalContexts)
-            {
-                foreach (NodeEvaluationContext? context in item)
-                {
-                    context.Target = target;
-                    context._renderables = list;
-                }
-            }
-
-            _evaluator.Evaluate();
+            _snapshot.Evaluate(target, list);
 
             // Todo: LayerOutputNodeに移動
             foreach (EngineObject item in list.Span)
             {
-                item.ZIndex = element.ZIndex;
-                item.TimeRange = new TimeRange(element.Start, element.Length);
+                item.ZIndex = ZIndex;
+                item.TimeRange = TimeRange;
             }
 
             return list;
@@ -49,5 +41,57 @@ public class ElementNodeTreeModel : NodeTreeModel
             list.Dispose();
             throw;
         }
+    }
+
+    protected override void OnAttachedToHierarchy(in HierarchyAttachmentEventArgs args)
+    {
+        base.OnAttachedToHierarchy(in args);
+        _element = this.FindHierarchicalParent<Element>();
+        if (_element != null)
+        {
+            _element.PropertyChanged += OnParentElementPropertyChanged;
+            UpdateValueFromElement();
+        }
+    }
+
+    private void UpdateValueFromElement()
+    {
+        if (_element == null) return;
+
+        IsTimeAnchor = true;
+        ZIndex = _element.ZIndex;
+        TimeRange = new TimeRange(_element.Start, _element.Length);
+        IsEnabled = _element.IsEnabled && IsEnabled;
+    }
+
+    private void OnParentElementPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e is CorePropertyChangedEventArgs args && _element != null)
+        {
+            if (args.Property.Id == Element.IsEnabledProperty.Id)
+            {
+                IsEnabled = _element.IsEnabled;
+            }
+            else if (args.Property.Id == Element.ZIndexProperty.Id)
+            {
+                ZIndex = _element.ZIndex;
+            }
+            else if (args.Property.Id == Element.StartProperty.Id ||
+                     args.Property.Id == Element.LengthProperty.Id)
+            {
+                TimeRange = new TimeRange(_element.Start, _element.Length);
+            }
+        }
+    }
+
+    protected override void OnDetachedFromHierarchy(in HierarchyAttachmentEventArgs args)
+    {
+        base.OnDetachedFromHierarchy(in args);
+        if (_element != null)
+        {
+            _element.PropertyChanged -= OnParentElementPropertyChanged;
+        }
+
+        _element = null;
     }
 }
