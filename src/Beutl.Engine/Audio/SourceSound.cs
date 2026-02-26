a353b217a633c132248ae02ed5ff3fd140694208
Yuto Terada <lugongdao07@gmail.com>
2026-02-27 02:41:34 +0900

    feat: add IElementThumbnailsProvider and IElementThumbnailCacheService interfaces with thumbnail handling in SourceSound and SourceVideo classes

--- src/Beutl.Engine/Audio/SourceSound.cs
+++ src/Beutl.Engine/Audio/SourceSound.cs
@@ -1,12 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Beutl.Engine;
+using Beutl.Graphics.Rendering;
 using Beutl.Language;
 using Beutl.Media.Source;
 
 namespace Beutl.Audio;
 
 [Display(Name = nameof(Strings.Sound), ResourceType = typeof(Strings))]
-public sealed class SourceSound : Sound
+public sealed partial class SourceSound : Sound
 {
     public SourceSound()
     {
