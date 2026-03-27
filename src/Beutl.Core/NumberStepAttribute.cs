f1906cbc5e849f92805bcc3ed3b0692a10308110
Yuto Terada <lugongdao07@gmail.com>
2026-03-27 23:43:09 +0900

    fix: remove invisible characters from namespace declarations in NumberEditorHelper and NumberStepAttribute

--- src/Beutl.Core/NumberStepAttribute.cs
+++ src/Beutl.Core/NumberStepAttribute.cs
@@ -1,4 +1,4 @@
-namespace Beutl;
+﻿namespace Beutl;
 
 [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
 public sealed class NumberStepAttribute(double largeChange = 10, double smallChange = 1) : Attribute
