9a797719d1a11d6aed7ff7e5a5f4989a77d466c5
Yuto Terada <lugongdao07@gmail.com>
2026-04-12 01:08:53 +0900

    refactor: remove keyboard focus handler from EditView constructor

--- src/Beutl/Views/EditView.axaml.cs
+++ src/Beutl/Views/EditView.axaml.cs
@@ -1,6 +1,4 @@
-﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.VisualTree;
 
 namespace Beutl.Views;
 
@@ -9,12 +7,5 @@ public sealed partial class EditView : UserControl
     public EditView()
     {
         InitializeComponent();
-
-        this.GetObservable(IsKeyboardFocusWithinProperty)
-            .Subscribe(v =>
-            {
-                var playerView = this.GetVisualDescendants().OfType<PlayerView>().FirstOrDefault();
-                playerView?.Player.SetSeekBarOpacity(v ? 1 : 0.8);
-            });
     }
 }
