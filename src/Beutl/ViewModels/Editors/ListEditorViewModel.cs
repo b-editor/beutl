2559b69bedce230299faa223bbf7a98423691095
Yuto Terada <lugongdao07@gmail.com>
2026-04-14 16:52:00 +0900

    refactor: add IsEnabled property and update IListEditorViewModel interface

--- src/Beutl/ViewModels/Editors/ListEditorViewModel.cs
+++ src/Beutl/ViewModels/Editors/ListEditorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections;
+using System.Collections.Specialized;
 using System.Text.Json.Nodes;
 using Beutl.Animation;
 using Beutl.Editor.Services;
@@ -80,6 +81,16 @@ public interface IListEditorViewModel
     bool IsListNull { get; }
 
     bool IsDisposed { get; }
+
+    string Header { get; }
+
+    ReactivePropertySlim<string?> Description { get; }
+
+    IReadOnlyReactiveProperty<IEnumerable?> List { get; }
+
+    ICoreReadOnlyList<IListItemEditorViewModel> Items { get; }
+
+    ReactivePropertySlim<bool> IsExpanded { get; }
 
     void MoveItem(int oldIndex, int newIndex);
 
@@ -187,7 +198,11 @@ public sealed class ListEditorViewModel<TItem> : BaseEditorViewModel, IListEdito
 
     public ReadOnlyReactivePropertySlim<IList<TItem?>?> List { get; }
 
+    IReadOnlyReactiveProperty<IEnumerable?> IListEditorViewModel.List => List;
+
     public CoreList<ListItemEditorViewModel<TItem>> Items { get; } = [];
+
+    ICoreReadOnlyList<IListItemEditorViewModel> IListEditorViewModel.Items => Items;
 
     public ReactivePropertySlim<bool> IsExpanded { get; } = new(false);
 
