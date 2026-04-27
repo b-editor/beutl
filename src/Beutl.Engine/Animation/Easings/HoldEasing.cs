c12ad602f58d082b86f4a98c58dcfcee42551a93
Yuto Terada <66758394+yuto-trd@users.noreply.github.com>
2026-04-27 22:24:14 +0900

    feat: Add Hold easing for step interpolation (#1721)
    * feat: add Hold easing for step interpolation
    
    * chore: format

---
+++ src/Beutl.Engine/Animation/Easings/HoldEasing.cs
@@ -0,0 +1,9 @@
+﻿namespace Beutl.Animation.Easings;
+
+public sealed class HoldEasing : Easing
+{
+    public override float Ease(float progress)
+    {
+        return progress < 1f ? 0f : 1f;
+    }
+}
---
+++ tests/Beutl.UnitTests/Engine/Animation/HoldEasingTests.cs
@@ -0,0 +1,32 @@
+﻿using Beutl.Animation.Easings;
+
+namespace Beutl.UnitTests.Engine.Animation;
+
+public class HoldEasingTests
+{
+    [Test]
+    public void Ease_BeforeEnd_ReturnsZero()
+    {
+        var easing = new HoldEasing();
+
+        Assert.That(easing.Ease(0f), Is.EqualTo(0f));
+        Assert.That(easing.Ease(0.5f), Is.EqualTo(0f));
+        Assert.That(easing.Ease(0.999f), Is.EqualTo(0f));
+    }
+
+    [Test]
+    public void Ease_AtEnd_ReturnsOne()
+    {
+        var easing = new HoldEasing();
+
+        Assert.That(easing.Ease(1f), Is.EqualTo(1f));
+    }
+
+    [Test]
+    public void Ease_BeyondEnd_ReturnsOne()
+    {
+        var easing = new HoldEasing();
+
+        Assert.That(easing.Ease(1.5f), Is.EqualTo(1f));
+    }
+}
