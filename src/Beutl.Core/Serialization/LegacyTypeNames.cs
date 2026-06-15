28c1c432f67f373a92c3154d9579d26cb32eb01b
Yuto Terada <66758394+yuto-trd@users.noreply.github.com>
2026-06-16 02:45:58 +0900

    refactor(core): extract fragile legacy type-name literals into LegacyTypeNames constants (#1925)

---
+++ src/Beutl.Core/Serialization/LegacyTypeNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Beutl.Serialization;
+
+internal static class LegacyTypeNames
+{
+    internal const string ElementFullName = "Beutl.ProjectSystem.Element";
+    internal const string SceneDiscriminator = "[Beutl.ProjectSystem]:Scene";
+    internal const string ElementDiscriminator = "[Beutl.ProjectSystem]:Element";
+}
---
+++ tests/Beutl.UnitTests/Core/LegacyTypeNamesTests.cs
@@ -0,0 +1,26 @@
+﻿using Beutl.ProjectSystem;
+using Beutl.Serialization;
+
+namespace Beutl.UnitTests.Core;
+
+[TestFixture]
+public class LegacyTypeNamesTests
+{
+    [Test]
+    public void ElementFullName_MatchesRuntimeType()
+    {
+        Assert.That(typeof(Element).FullName, Is.EqualTo(LegacyTypeNames.ElementFullName));
+    }
+
+    [Test]
+    public void SceneDiscriminator_MatchesTypeFormat()
+    {
+        Assert.That(TypeFormat.ToString(typeof(Scene)), Is.EqualTo(LegacyTypeNames.SceneDiscriminator));
+    }
+
+    [Test]
+    public void ElementDiscriminator_MatchesTypeFormat()
+    {
+        Assert.That(TypeFormat.ToString(typeof(Element)), Is.EqualTo(LegacyTypeNames.ElementDiscriminator));
+    }
+}
