356c55734c546c11309037b59f39c90be6d57040
Yuto Terada <lugongdao07@gmail.com>
2026-06-27 21:50:56 +0900

    feat: improve proxy tab UI localization

---
+++ src/Beutl.Editor.Components/ProxiesTab/Views/ProxyPresetIndexConverter.cs
@@ -0,0 +1,32 @@
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Beutl.Media.Proxy;
+
+namespace Beutl.Editor.Components.ProxiesTab.Views;
+
+public sealed class ProxyPresetIndexConverter : IValueConverter
+{
+    public static readonly ProxyPresetIndexConverter Instance = new();
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            ProxyPreset.Half => 0,
+            ProxyPreset.Quarter => 1,
+            ProxyPreset.Eighth => 2,
+            _ => 1,
+        };
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            0 => ProxyPreset.Half,
+            1 => ProxyPreset.Quarter,
+            2 => ProxyPreset.Eighth,
+            _ => ProxyPreset.Quarter,
+        };
+    }
+}
