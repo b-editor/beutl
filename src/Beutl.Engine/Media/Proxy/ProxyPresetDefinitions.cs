2f1c5de5e29f570b83cccd1dfd6ffd34f11e8c8c
Yuto Terada <lugongdao07@gmail.com>
2026-06-28 10:04:21 +0900

    fix: apply dotnet format

--- src/Beutl.Engine/Media/Proxy/ProxyPresetDefinitions.cs
+++ src/Beutl.Engine/Media/Proxy/ProxyPresetDefinitions.cs
@@ -14,11 +14,11 @@ public static class ProxyPresetDefinitions
     private static readonly IReadOnlyDictionary<ProxyPreset, ProxyEncodeParameters> s_parameters =
         new ReadOnlyDictionary<ProxyPreset, ProxyEncodeParameters>(
             new Dictionary<ProxyPreset, ProxyEncodeParameters>
-        {
-            [ProxyPreset.Half] = new(0.5f, 25, 1920, "fastdecode", "fast"),
-            [ProxyPreset.Quarter] = new(0.25f, 26, 1280, "fastdecode", "fast"),
-            [ProxyPreset.Eighth] = new(0.125f, 28, 960, "fastdecode", "fast"),
-        });
+            {
+                [ProxyPreset.Half] = new(0.5f, 25, 1920, "fastdecode", "fast"),
+                [ProxyPreset.Quarter] = new(0.25f, 26, 1280, "fastdecode", "fast"),
+                [ProxyPreset.Eighth] = new(0.125f, 28, 960, "fastdecode", "fast"),
+            });
 
     public static ProxyEncodeParameters Get(ProxyPreset preset)
     {
