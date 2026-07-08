4d8875b6cfaf0f6ba3cb58a89ebe4516ea969f2d
Yuto Terada <lugongdao07@gmail.com>
2026-07-08 19:43:22 +0900

    style(test): add UTF-8 BOM to new EffectPipeline test files

--- tests/Beutl.UnitTests/Engine/Graphics/Rendering/EffectPipeline/CompositeDensityTests.cs
+++ tests/Beutl.UnitTests/Engine/Graphics/Rendering/EffectPipeline/CompositeDensityTests.cs
@@ -1,4 +1,4 @@
-using Beutl.Graphics;
+﻿using Beutl.Graphics;
 using Beutl.Graphics.Effects;
 using Beutl.Graphics.Rendering;
 using Beutl.Logging;
--- tests/Beutl.UnitTests/Engine/Graphics/Rendering/EffectPipeline/RoiRelativeBakeTests.cs
+++ tests/Beutl.UnitTests/Engine/Graphics/Rendering/EffectPipeline/RoiRelativeBakeTests.cs
@@ -1,4 +1,4 @@
-using Beutl.Composition;
+﻿using Beutl.Composition;
 using Beutl.Graphics;
 using Beutl.Graphics.Backend;
 using Beutl.Graphics.Effects;
