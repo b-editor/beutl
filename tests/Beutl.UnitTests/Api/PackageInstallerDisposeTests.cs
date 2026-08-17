8c8bbe2acc15c4cf3a15e57e1b00ef78c35d4ba0
Yuto Terada <lugongdao07@gmail.com>
2026-08-17 10:43:43 +0900

    style: apply dotnet format to review changes

--- tests/Beutl.UnitTests/Api/PackageInstallerDisposeTests.cs
+++ tests/Beutl.UnitTests/Api/PackageInstallerDisposeTests.cs
@@ -1,4 +1,4 @@
-using System.Net;
+﻿using System.Net;
 using Beutl.Api;
 using Beutl.Api.Services;
 using Beutl.Testing.Headless;
--- tests/Beutl.UnitTests/Api/ProfileLifetimeTests.cs
+++ tests/Beutl.UnitTests/Api/ProfileLifetimeTests.cs
@@ -1,4 +1,4 @@
-using System.Net;
+﻿using System.Net;
 using System.Net.Http.Json;
 using Beutl.Api;
 using Beutl.Api.Clients;
